fb4e6c112aab8877d93573b654866a53e6a2c076
John Vinyard <john.vinyard@gmail.com>
2021-08-22 10:29:34 -0500

    Discriminator makes a single judgement for each segment

--- modules2.py
+++ modules2.py
@@ -17,7 +17,7 @@ def init_weights(p):
 
     with torch.no_grad():
         try:
-            p.weight.uniform_(-0.1, 0.1)
+            p.weight.uniform_(-0.05, 0.05)
         except AttributeError:
             pass
 
--- modules4.py
+++ modules4.py
@@ -62,7 +62,7 @@ class Discriminator(nn.Module):
         self.atom_embedding.requires_grad = False
 
         self.atom_judge = LinearOutputStack(
-            channels, 3, out_channels=1, in_channels=10)
+            channels, 3, in_channels=10)
 
         self.dense = nn.Sequential(
             LinearOutputStack(channels, 2, in_channels=10,
@@ -80,7 +80,10 @@ class Discriminator(nn.Module):
 
         self.length = LinearOutputStack(
             channels, 2, in_channels=1, activation=activation)
-        self.final = nn.Linear(channels * 2, 1)
+        self.final = nn.Linear(channels * 2, channels)
+
+        self.weighting = LinearOutputStack(channels, 3, out_channels=1, in_channels=channels * 2)
+        self.final_final = LinearOutputStack(channels, 3, out_channels=1)
 
         self.apply(init_weights)
 
@@ -103,7 +106,7 @@ class Discriminator(nn.Module):
         if isinstance(x, list):
             x = self.get_embeddings(x)
 
-        aj = self.atom_judge(x).view(-1, 1)
+        aj = self.atom_judge(x)
 
         l = self.length(l).repeat(x.shape[0], 1)
         x = self.dense(x)
@@ -111,8 +114,16 @@ class Discriminator(nn.Module):
         x = torch.cat([
             x.view(-1, self.channels),
             l.view(-1, self.channels)], dim=1)
-        x = self.final(x).view(-1, 1)
+        x = self.final(x)
+
         x = torch.cat([x, aj], dim=1)
+
+        w = self.weighting(x)
+        x = w * x
+
+        # x = torch.max(x, dim=1)[0]
+        # x = self.final_final(x)
+
         return x
 
 
