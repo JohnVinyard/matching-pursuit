d5fcbfe9a8f0fbc0ef251b8b37df4f3b3fe83293
John Vinyard <john.vinyard@gmail.com>
2021-11-22 07:59:54 -0600

    GAN setting for siren experiment

--- siren.py
+++ siren.py
@@ -1,4 +1,5 @@
 from typing import ForwardRef
+from scipy.sparse.construct import random
 import torch
 from torch.optim.adam import Adam
 import zounds
@@ -8,16 +9,16 @@ from datastore import batch_stream
 from modules import pos_encode_feature
 from modules3 import LinearOutputStack
 from torch.nn import functional as F
-from itertools import chain
 import numpy as np
 from scipy.fftpack import dct, idct
 
 sr = zounds.SR22050()
 batch_size = 2
 n_samples = 2**15
-overfit = False
+n_sub_samples = 4096
 do_cumsum = False
-final_activation = True
+final_activation = False
+n_embedding_freqs = 16
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -39,54 +40,116 @@ def init_weights(p):
             pass
 
 
+def init_disc_weights(p):
+    with torch.no_grad():
+        try:
+            p.weight.uniform_(-0.1, 0.1)
+        except AttributeError:
+            pass
+
+
 def activation(x):
     return torch.sin(x)
     # return F.leaky_relu(x, 0.2)
 
+class Activation(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        return activation(x)
+
+
 def transform(x):
-    return x
-    # return dct(x, axis=-1, norm='ortho')
+    # return x
+    return dct(x, axis=-1, norm='ortho')
+
 
 def inverse_transform(x):
-    return x
-    # return idct(x, axis=-1, norm='ortho')
+    # return x
+    return idct(x, axis=-1, norm='ortho')
 
 
-class Encoder(nn.Module):
+# class Encoder(nn.Module):
+#     def __init__(self, channels):
+#         super().__init__()
+
+#         self.embed_pos = nn.Conv1d(33, 8, 1, 1, 0, bias=False)
+#         self.embed_sample = nn.Conv1d(1, 8, 7, 1, 3, bias=False)
+
+#         self.factor = nn.Parameter(torch.FloatTensor(1).fill_(10))
+
+#         self.net = nn.Sequential(
+#             nn.Conv1d(16, 16, 8, 8, 0, bias=False),
+#             nn.Conv1d(16, 32, 8, 8, 0, bias=False),
+#             nn.Conv1d(32, 64, 8, 8, 0, bias=False),
+#             nn.Conv1d(64, 128, 8, 8, 0, bias=False),
+#             nn.Conv1d(128, 128, 8, 8, 0, bias=False),
+#         )
+
+#         self.apply(init_weights)
+
+#     def forward(self, pos, sample):
+
+#         pos = pos.permute(0, 2, 1)
+#         sample = sample.permute(0, 2, 1)
+
+#         pe = self.embed_pos(pos)
+#         se = self.embed_sample(sample)
+
+#         x = torch.cat([pe, se], dim=1)
+#         x = activation(x * self.factor)
+
+#         for layer in self.net:
+#             x = layer(x)
+#             x = activation(x * self.factor)
+
+#         x = x.view(-1, 128)
+#         return x
+
+
+class Disc(nn.Module):
     def __init__(self, channels):
         super().__init__()
-
-        self.embed_pos = nn.Conv1d(33, 8, 1, 1, 0, bias=False)
-        self.embed_sample = nn.Conv1d(1, 8, 7, 1, 3, bias=False)
-
-        self.factor = nn.Parameter(torch.FloatTensor(1).fill_(10))
+        self.channels = channels
+        self.pos_embed = LinearOutputStack(
+            channels, 3, in_channels=n_embedding_freqs * 2 + 1, activation=activation)
+        self.sample_embed = LinearOutputStack(channels, 3, in_channels=1, activation=activation)
+        self.comb = LinearOutputStack(channels, 3, in_channels=channels * 2, activation=activation)
 
         self.net = nn.Sequential(
-            nn.Conv1d(16, 16, 8, 8, 0, bias=False),
-            nn.Conv1d(16, 32, 8, 8, 0, bias=False),
-            nn.Conv1d(32, 64, 8, 8, 0, bias=False),
-            nn.Conv1d(64, 128, 8, 8, 0, bias=False),
-            nn.Conv1d(128, 128, 8, 8, 0, bias=False),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, channels, 7, 2, 3),
+            Activation(),
+            nn.Conv1d(channels, 1, 1, 1, 0)
         )
-
-        self.apply(init_weights)
+        self.apply(init_disc_weights)
 
     def forward(self, pos, sample):
+        p = self.pos_embed(pos)
+        s = self.sample_embed(sample)
 
-        pos = pos.permute(0, 2, 1)
-        sample = sample.permute(0, 2, 1)
+        x = torch.cat([p, s], dim=-1)
+        x = self.comb(x)
 
-        pe = self.embed_pos(pos)
-        se = self.embed_sample(sample)
-
-        x = torch.cat([pe, se], dim=1)
-        x = activation(x * self.factor)
-
-        for layer in self.net:
-            x = layer(x)
-            x = activation(x * self.factor)
-
-        x = x.view(-1, 128)
+        x = x.permute(0, 2, 1)
+        x = self.net(x)
+        x = x.permute(0, 2, 1)
         return x
 
 
@@ -96,12 +159,13 @@ class Layer(nn.Module):
         self.linear = nn.Linear(in_channels, out_channels, bias=True)
         self.factor = nn.Parameter(torch.FloatTensor(out_channels).fill_(10))
         self.apply_activation = apply_activation
+        self.out_channels = out_channels
 
     def forward(self, x, f, b):
         x = self.linear(x)
         if self.apply_activation:
             # f = activation(f)
-            v = (self.factor * x) + b[:, None, :]
+            v = (self.factor * x) + b.view(batch_size, 1, -1)
             x = activation(v)
         return x
 
@@ -128,13 +192,11 @@ class Network(nn.Module):
             Layer(hidden_channels, 1, apply_activation=final_activation)
         )
 
-
         self.apply(init_weights)
 
     def forward(self, x, latent):
-
         pb = self.pos_bias(latent)
-        x = x + pb[:, None, :]
+        x = x + pb
 
         for i, layer in enumerate(self.net):
             try:
@@ -148,20 +210,31 @@ class Network(nn.Module):
                 bias = torch.zeros(batch_size, 1).to(x.device)
                 x = layer(x, factor, bias)
 
-
         # x = x * omegas
         # x = torch.sigmoid(x) * np.pi
         if do_cumsum:
             x = torch.sin(torch.cumsum(x, dim=-1))
+        
+        # x = x - x.mean()
         return x
 
 
-def to_pairs(signal):
+def to_pairs(signal, random_indices=True):
+
+    if random_indices:
+        indices = torch.randperm(n_samples)[:n_sub_samples]
+    else:
+        indices = torch.arange(n_samples)
 
     signal = torch.from_numpy(signal).to(device)
+    signal = signal.permute(1, 0)
+    signal = signal[indices]
+    signal = signal.permute(1, 0)
 
-    pos = torch.linspace(-1, 1, n_samples).view(-1, 1).to(device)
-    pos = pos_encode_feature(pos, 1, n_samples, 16).repeat(batch_size, 1, 1)
+    pos = torch.linspace(-1, 1, n_samples).view(-1, 1).to(device)[indices]
+
+    pos = pos_encode_feature(
+        pos, 1, n_samples, n_embedding_freqs).repeat(batch_size, 1, 1)
 
     samples = signal[..., None]
 
@@ -169,47 +242,67 @@ def to_pairs(signal):
 
 
 def real():
-    index = np.random.randint(0, batch_size)
-    return zounds.AudioSamples(inverse_transform(samples[index].data.cpu().numpy().reshape(-1)), sr).pad_with_silence()
+    _, samples = get_batch(random_indices=False, batch_size=1)
+    return zounds.AudioSamples(inverse_transform(samples.data.cpu().numpy().reshape(-1)), sr).pad_with_silence()
 
 
 def fake():
-    index = np.random.randint(0, batch_size)
-    return zounds.AudioSamples(inverse_transform(recon[index].data.cpu().numpy().reshape(-1)), sr).pad_with_silence()
+    with torch.no_grad():
+        pos, _ = get_batch(random_indices=False, batch_size=1)
+        z = latent()
+        samples = gen(pos, z)
+    return zounds.AudioSamples(inverse_transform(samples.data.cpu().numpy().reshape(-1)), sr).pad_with_silence()
+
+
+def latent():
+    return torch.FloatTensor(batch_size, 1, 128).normal_(0, 1).to(device)
+
+
+def train_gen(pos, samples):
+    
+    gen_optim.zero_grad()
+    z = latent()
+    fake = gen(pos, z)
+    fj = disc(pos, fake)
+    loss = torch.abs(1 - fj).mean()
+    loss.backward()
+    gen_optim.step()
+    print('G', loss.item())
+
+
+def train_disc(pos, samples):
+    disc_optim.zero_grad()
+    z = latent()
+    fake = gen(pos, z)
+    rj = disc(pos, samples)
+    fj = disc(pos, fake)
+    loss = (torch.abs(rj - 1).mean() + torch.abs(fj - 0).mean()) * 0.5
+    loss.backward()
+    disc_optim.step()
+    print('D', loss.item())
+
+
+def get_batch(random_indices=True, batch_size=batch_size):
+    sig = next(batch_stream(path, '*.wav', batch_size, n_samples))
+    sig /= (sig.max(axis=-1, keepdims=True) + 1e-12)
+    sig = transform(sig)
+    pos, samples = to_pairs(sig, random_indices)
+    return pos, samples
 
 
 if __name__ == '__main__':
     app = zounds.ZoundsApp(locals=locals(), globals=globals())
     app.start_in_thread(9999)
 
-    encoder = Encoder(128).to(device)
-    net = Network(6, 33, 128).to(device)
-    optim = Adam(chain(net.parameters(), encoder.parameters()),
-                 lr=1e-4, betas=(0, 0.9))
-
-    
-    sig = next(batch_stream(path, '*.wav', batch_size, n_samples))
-    sig /= (sig.max(axis=-1, keepdims=True) + 1e-12)
-    sig = transform(sig)
+    gen = Network(6, 33, 128).to(device)
+    gen_optim = Adam(gen.parameters(), lr=1e-4, betas=(0, 0.9))
+    disc = Disc(128).to(device)
+    disc_optim = Adam(disc.parameters(), lr=1e-4, betas=(0, 0.9))
 
     while True:
-        optim.zero_grad()
 
-        if not overfit:
-            sig = next(batch_stream(path, '*.wav', batch_size, n_samples))
-            sig /= (sig.max(axis=-1, keepdims=True) + 1e-12)
-            sig = transform(sig)
+        pos, samples = get_batch()
+        train_disc(pos, samples)
 
-        pos, samples = to_pairs(sig)
-
-        latent = encoder(pos, samples)
-        z = latent.data.cpu().numpy().squeeze()
-        recon = net(pos, latent)
-
-        recon = recon.view(batch_size, n_samples)
-        samples = samples.view(batch_size, n_samples)
-
-        loss = F.mse_loss(recon, samples)
-        loss.backward()
-        optim.step()
-        print(loss.item())
+        pos, samples = get_batch()
+        train_gen(pos, samples)
