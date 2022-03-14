df5be6405bf332e4009d6b9e41001993f5fbd289
John Vinyard <johnwho@gmail.com>
2022-03-14 07:53:29 -0500

    exploring options in NERF-like model

--- nerf.py
+++ nerf.py
@@ -19,59 +19,80 @@ from util.weight_init import make_initializer
 '''
 Questions to answer:
 
+
+
 - are negative phases meaningful?  Are there other alternatives?
 - can I backprop through angle() satisfactorily (understand cosine and sine)
-- Can I learn 1d (YES) or 2d NERF representations of the new spectrogram representation
 - Can I create an adaptible NERF model (i.e., NERF autoencoder)
+
+Answered:
+- Can I learn 1d (YES) or 2d NERF representations of the new spectrogram representation
+
 '''
 
 init_weights = make_initializer(0.1)
 
 def activation(x):
-    # return F.leaky_relu(x, 0.2)
-    return torch.sin(x)
+    return F.leaky_relu(x, 0.2)
 
 
-class ModulatedLayer(nn.Module):
-    def __init__(self, channels, forward_layers, conditioning_layers):
-        super().__init__()
-        self.f = LinearOutputStack(channels, forward_layers, activation=activation)
-        self.weight = LinearOutputStack(channels, conditioning_layers, activation=activation)
-        self.bias = LinearOutputStack(channels, conditioning_layers, activation=activation)
+# class ModulatedLayer(nn.Module):
+#     def __init__(self, channels, forward_layers, conditioning_layers):
+#         super().__init__()
+#         self.f = LinearOutputStack(channels, forward_layers, activation=activation)
+#         self.weight = LinearOutputStack(channels, conditioning_layers, activation=activation)
+#         self.bias = LinearOutputStack(channels, conditioning_layers, activation=activation)
 
-    def forward(self, x, conditioning):
-        x = self.f(x)
-        w = self.weight(conditioning)
-        b = self.bias(conditioning)
+#     def forward(self, x, conditioning):
+#         x = self.f(x)
+#         w = self.weight(conditioning)
+#         b = self.bias(conditioning)
 
-        return (x * torch.sigmoid(w)) + b
+#         return (x * torch.sigmoid(w)) + b
 
 
-class ModulatedStack(nn.Module):
-    def __init__(self, channels, layers, freq_bins):
-        super().__init__()
-        self.initial = LinearOutputStack(channels, 1, in_channels=33, activation=activation)
-        self.net = nn.Sequential(
-            *[ModulatedLayer(channels, 2, 2) for _ in range(layers)])
-        self.mag = LinearOutputStack(channels, 3, out_channels=freq_bins, activation=activation)
-        self.phase = LinearOutputStack(channels, 3, out_channels=freq_bins, activation=activation)
+# class ModulatedStack(nn.Module):
+#     def __init__(self, channels, layers, freq_bins, pos_encoder):
+#         super().__init__()
 
-    def forward(self, latent):
-        pos = pos_encoded(latent.shape[0], 128, 16, device)
-        x = self.initial(pos)
+#         self.pos_encoder = pos_encoder
+#         self.initial = LinearOutputStack(channels, 1, in_channels=33, activation=activation)
+#         self.net = nn.Sequential(
+#             *[ModulatedLayer(channels, 2, 2) for _ in range(layers)])
+#         self.mag = LinearOutputStack(channels, 3, out_channels=freq_bins, activation=activation)
+#         self.phase = LinearOutputStack(channels, 3, out_channels=freq_bins, activation=activation)
 
-        for layer in self.net:
-            x = layer(x, latent[:, None, :])
+#     def forward(self, latent):
+#         pos = self.pos_encoder(latent.shape[0], 128, 16, device)
+#         x = self.initial(pos)
 
-        mag = self.mag(x)
-        phase = self.phase(x)
-        x = torch.cat([mag[..., None], phase[..., None]], dim=-1)
-        return x
+#         for layer in self.net:
+#             x = layer(x, latent[:, None, :])
+
+#         mag = self.mag(x)
+#         phase = self.phase(x)
+#         x = torch.cat([mag[..., None], phase[..., None]], dim=-1)
+#         return x
+
+
+def binary_pos_encoded(batch_size, time_dim, n_freqs, device):
+    n_freqs = (n_freqs * 2) + 1
+    pos = torch.zeros(batch_size, time_dim, n_freqs).to(device)
+    for i in range(n_freqs, 1, -1):
+        pos[:, ::i, i - 1] = 1
+    return pos
+
+
+def reduce_mean(x):
+    return x.mean(dim=1)
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self, pos_encoder, reduction):
         super().__init__()
+
+        self.pos_encoder = pos_encoder
+        self.reduction = reduction
 
         self.embed_pos = LinearOutputStack(128, 2, in_channels=33, out_channels=64, activation=activation)
 
@@ -80,15 +101,16 @@ class Encoder(nn.Module):
         self.embed_phase = LinearOutputStack(
             128, 2, in_channels=256, out_channels=64, activation=activation)
         self.down = nn.Linear(64 * 3, 128)
-        # self.t = Transformer(128, 5)
         self.t = Mixer(128, 128, 5)
         self.final = LinearOutputStack(128, 2, activation=activation)
+
+        self.attention = LinearOutputStack(128, 2, out_channels=1)
         self.apply(init_weights)
 
     def forward(self, x):
         batch, time, channels, _ = x.shape
 
-        pos = pos_encoded(batch, 128, 16, x.device)
+        pos = self.pos_encoder(batch, 128, 16, x.device)
         pos = self.embed_pos(pos)
 
         mag = x[..., 0]
@@ -99,26 +121,35 @@ class Encoder(nn.Module):
         x = self.down(x)
 
         x = self.t(x)
-        x = x[:, -1, :]
+
+        attn = self.attention(x)
+
+        x = self.reduction(x * attn)
         x = self.final(x)
         return x
 
 
 class OneDNerf(nn.Module):
-    def __init__(self):
+    def __init__(self, pos_encoder):
         super().__init__()
         self.embed = nn.Linear(33, 128)
-        self.net = LinearOutputStack(128, 5, activation=activation)
+        self.net = LinearOutputStack(128, 5, activation=activation, in_channels=128 * 2)
         self.mag = LinearOutputStack(128, 3, out_channels=256, activation=activation)
         self.phase = LinearOutputStack(128, 3, out_channels=256, activation=activation)
+        self.pos_encoder = pos_encoder
         self.apply(init_weights)
 
-    def forward(self, x, conditioning=None):
-        pos = pos_encoded(1, 128, 16, device)
+    def forward(self, conditioning):
+        """
+        Note: typically positional encoding would be passed
+        in, but why not just generate it here, since it's static
+        """
+        pos = self.pos_encoder(1, 128, 16, device).repeat(conditioning.shape[0], 1, 1)
         pos = self.embed(pos)
 
         if conditioning is not None:
-            pos = conditioning + pos
+            conditioning = conditioning[:, None, :].repeat(1, pos.shape[1], 1)
+            pos = torch.cat([conditioning, pos], dim=-1)
         
         x = self.net(pos)
         mag = self.mag(x) ** 2
@@ -148,11 +179,13 @@ def stream(batch_size, n_samples, overfit=False):
             batch = preprocess(next(bs), n_samples)
 
 
+pe = binary_pos_encoded
+
 codec = AudioCodec(MelScale())
-model = OneDNerf().to(device)
-# model = ModulatedStack(128, 5, 256).to(device)
-encoder = Encoder().to(device)
-optim = Adam(model.parameters(), lr=1e-3, betas=(0, 0.9))
+model = OneDNerf(pos_encoder=pe).to(device)
+# model = ModulatedStack(128, 5, 256, pos_encoder=pe).to(device)
+encoder = Encoder(pos_encoder=pe, reduction=reduce_mean).to(device)
+optim = Adam(model.parameters(), lr=1e-4, betas=(0, 0.9))
 
 def train_ae(batch):
     optim.zero_grad()
@@ -169,7 +202,6 @@ def train_ae(batch):
 if __name__ == '__main__':
     app = zounds.ZoundsApp(locals=locals(), globals=globals())
     app.start_in_thread(9999)
-
     
     batch_size = 4
     n_samples = 2 ** 15
@@ -177,6 +209,9 @@ if __name__ == '__main__':
 
     data_stream = stream(1 if overfit else batch_size, n_samples, overfit)
 
+    def pos_encoding():
+        pos = pe(batch_size, 128, 16, device)
+        return pos[0].data.cpu().numpy()
 
     def real_mag():
         return item[0, :, :, 0].data.cpu().numpy()
