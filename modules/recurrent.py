397b76550f0c7e24f5d78874502fec79315e7fd1
John Vinyard <johnwho@gmail.com>
2022-05-20 06:51:14 -0500

    WIP instruments

--- modules/recurrent.py
+++ modules/recurrent.py
@@ -1,14 +1,107 @@
 import torch
 from torch import nn
-
-
+from modules.atoms import unit_norm
+from modules.ddsp import NoiseModel, OscillatorBank
 from modules.linear import LinearOutputStack
+from util.weight_init import make_initializer
+
+
+def activation(x):
+    return torch.sin(x * 30)
 
 
 class RecurrentSynth(nn.Module):
-    def __init__(self, layers, channels):
+    def __init__(self, layers, channels, is_silent=False, silent_factor=1e-6):
         super().__init__()
+        self.net = LinearOutputStack(
+            channels, layers, activation=activation)
+        self.gate = LinearOutputStack(
+            channels, layers, out_channels=1, activation=activation)
+        self.is_silent = is_silent
+        self.silent_factor = silent_factor
+
+    def forward(self, x, max_iter=10):
+        results = []
+        x = unit_norm(x)
+        for i in range(max_iter):
+            x = self.net(x)
+            x = unit_norm(x)
+            results.append(x[..., None])
+
+        x = torch.cat(results, dim=-1)
+
+        z = x.permute(0, 2, 1)
+        z = torch.relu(self.gate(z))
+        z = z.permute(0, 2, 1)
+
+        if self.is_silent:
+            z = z * self.silent_factor
+
+        return x, z
+
+# (batch, voices, time, channels)
+
+
+class Conductor(nn.Module):
+    def __init__(self, layers, channels, voices):
+        super().__init__()
+        self.layers = layers
+        self.channels = channels
+        self.voices = voices
+
+        instruments = \
+            [RecurrentSynth(layers, channels) for _ in range(self.voices)] \
+            + [(RecurrentSynth(layers, channels, is_silent=True))]
+        
+        self.instruments = nn.ParameterList(instruments)
+
         self.net = LinearOutputStack(channels, layers)
-    
+        self.router = LinearOutputStack(
+            channels, layers, out_channels=self.voices + 1)
+        
+        # self.osc = OscillatorBank(
+        #     input_channels=128,
+        #     n_osc=128,
+        #     n_audio_samples=n_samples,
+        #     activation=torch.sigmoid,
+        #     amp_activation=torch.abs,
+        #     return_params=True,
+        #     constrain=True,
+        #     log_frequency=False,
+        #     lowest_freq=40 / samplerate.nyquist,
+        #     sharpen=False,
+        #     compete=False)
+
+        # self.noise = NoiseModel(
+        #     input_channels=128,
+        #     input_size=128,
+        #     n_noise_frames=512,
+        #     n_audio_samples=n_samples,
+        #     channels=128,
+        #     activation=lambda x: x,
+        #     squared=False,
+        #     mask_after=1,
+        #     return_params=True)
+
     def forward(self, x):
-        pass
+        batch, time, channels = x.shape
+        if batch != 1:
+            raise NotImplementedError('Batch mode not implemented')
+        
+        x = x.view(time, channels)
+
+        x = self.net(x)
+
+        routes = self.router(x)
+        routes = torch.softmax(routes, dim=-1)
+        indices = torch.argmax(routes, dim=1)
+
+        for i in range(time):
+            r = routes[indices[i]]
+
+            if r == self.voices:
+                continue
+
+            inst = self.instruments[r]
+            params, env = inst.forward(x[i], time - i)
+            # TODO: Pass params to synthesizer and apply envelope =)-hi
--- scratch2.py
+++ scratch2.py
@@ -1,60 +1,21 @@
 import torch
-from modules.atoms import AudioEvent
 import zounds
 import numpy as np
-from modules.ddsp import band_filtered_noise
 from modules.normal_pdf import pdf
+from modules.recurrent import RecurrentSynth
 
-from util import playable
 
 if __name__ == '__main__':
 
     app = zounds.ZoundsApp(globals=globals(), locals=locals())
     app.start_in_thread(9999)
 
-    n_events = 1
-    batch_size = 1
-    sequence_length = 64
-    n_harmonics = 32
-    sr = zounds.SR22050()
-    n_samples = 2**14
+    rec = RecurrentSynth(3, 128)
+    start = torch.zeros(1, 128).normal_(0, 1)
 
-    ae = AudioEvent(
-        sequence_length=sequence_length,
-        n_samples=2**14,
-        n_events=n_events,
-        min_f0=20,
-        max_f0=800,
-        n_harmonics=n_harmonics,
-        sr=sr)
+    hidden, gate = rec.forward(start, max_iter=20)
+    hidden = hidden.data.cpu().numpy().squeeze().T
+    gate = gate.data.cpu().numpy().squeeze()
+    print(hidden.shape, gate.shape)
 
-
-    f0 = torch.zeros(batch_size, n_events, sequence_length).fill_(0.1)
-    osc_env = torch.linspace(1, 0, sequence_length).view(1, 1, sequence_length) ** 2
-
-    harm_env = torch\
-        .linspace(0.1, 0, n_harmonics)\
-        .view(1, 1, n_harmonics, 1)\
-        .repeat(1, 1, 1, sequence_length) ** 2
-
-    
-    noise_env = torch.linspace(1, 0, sequence_length).view(1, 1, sequence_length) ** 2
-    noise_std = torch.zeros(batch_size, n_events, sequence_length).fill_(0.5)
-
-    overall_env = torch.zeros(batch_size, n_events, sequence_length).fill_(0)
-
-    result = ae.forward(f0, overall_env, osc_env, noise_env, harm_env, noise_std)
-    audio = playable(result, sr)
-    spec = np.abs(zounds.spectral.stft(audio))
-
-
-    # batch, atoms, time
-    # mean = torch.linspace(0.001, 0.1, 64).view(1, 1, 64).repeat(2, 3, 1)
-    # std = torch.linspace(0.5, 0.001, 64).view(1, 1, 64).repeat(2, 3, 1)
-    # filt = pdf(torch.arange(0, 257, 1).view(1, 1, 257, 1), mean, std)
-    # filt = filt.data.cpu().numpy()
-
-    # noise = band_filtered_noise(n_samples, mean=mean, std=std)
-    # audio = playable(noise.view(-1, n_samples), sr)
-    # spec = np.abs(zounds.spectral.stft(audio))
     input('Waiting...')
