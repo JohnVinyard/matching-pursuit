b51209a6f7d256dca6553589ed037838039732b6
John Vinyard <johnwho@gmail.com>
2022-05-21 18:24:49 -0500

    First draft of sparse synth

--- modules/recurrent.py
+++ modules/recurrent.py
@@ -1,107 +1,185 @@
 import torch
 from torch import nn
 from modules.atoms import unit_norm
-from modules.ddsp import NoiseModel, OscillatorBank
+from modules.ddsp import noise_bank2
 from modules.linear import LinearOutputStack
+from modules.reverb import NeuralReverb
 from util.weight_init import make_initializer
+import numpy as np
+from torch.nn import functional as F
 
+init_weights = make_initializer(0.01)
 
 def activation(x):
     return torch.sin(x * 30)
 
 
+class Synth(nn.Module):
+    def __init__(self, layers, channels, samples_per_frame, n_osc=64):
+        super().__init__()
+        self.layers = layers
+        self.channels = channels
+        self.samples_per_frame = samples_per_frame
+        self.n_coeffs = ((samples_per_frame * 2) // 2) + 1
+
+        self.to_noise_params = LinearOutputStack(channels, layers, out_channels=self.n_coeffs)
+        self.n_osc = n_osc
+
+        # (batch, time, channels, 2)
+        self.to_osc_params = LinearOutputStack(channels, layers, out_channels=2 * self.n_osc)
+    
+    def forward(self, x):
+        batch, time, channels = x.shape
+        if batch != 1:
+            raise NotImplementedError('Only batch size of 1 is supported')
+        
+        noise = self.to_noise_params(x).permute(0, 2, 1)
+        noise = noise_bank2(noise)
+
+        osc = self.to_osc_params(x).view(batch, time, self.n_osc, 2)
+        amp = torch.norm(osc, dim=-1).permute(0, 2, 1)
+        freq = (torch.angle(torch.complex(osc[..., 0], osc[..., 1])) / np.pi).permute(0, 2, 1)
+
+        amp = F.interpolate(amp, size=self.samples_per_frame * time, mode='linear')
+        freq = F.interpolate(freq, size=self.samples_per_frame * time, mode='linear')
+
+        osc = torch.sin(torch.cumsum(freq * np.pi, dim=-1)) * amp
+        osc = torch.sum(osc, dim=1, keepdim=True)
+        signal = osc + noise
+        return signal
+        
+
 class RecurrentSynth(nn.Module):
-    def __init__(self, layers, channels, is_silent=False, silent_factor=1e-6):
+    def __init__(self, layers, channels, samples_per_frame):
         super().__init__()
         self.net = LinearOutputStack(
             channels, layers, activation=activation)
-        self.gate = LinearOutputStack(
-            channels, layers, out_channels=1, activation=activation)
-        self.is_silent = is_silent
-        self.silent_factor = silent_factor
+        self.gate = LinearOutputStack(channels, layers, out_channels=2)
+
+        mask = torch.zeros(2, channels)
+        mask[0, :] = 1
+
+        self.samples_per_frame = samples_per_frame
+
+        self.synth = Synth(layers, channels, samples_per_frame, n_osc=64)
+
+        self.register_buffer('mask', mask)
+
 
     def forward(self, x, max_iter=10):
         results = []
         x = unit_norm(x)
+
         for i in range(max_iter):
             x = self.net(x)
             x = unit_norm(x)
-            results.append(x[..., None])
+            g = torch.softmax(self.gate(x), dim=-1)
+            mask = g @ self.mask
+            x = x * mask
+            results.append(x[None, ...])
 
-        x = torch.cat(results, dim=-1)
+            index = torch.argmax(g, dim=-1)
+            if index.item() == 1:
+                break
 
-        z = x.permute(0, 2, 1)
-        z = torch.relu(self.gate(z))
-        z = z.permute(0, 2, 1)
 
-        if self.is_silent:
-            z = z * self.silent_factor
+        x = torch.cat(results, dim=0)[None, ...]
 
-        return x, z
+        signal = self.synth.forward(x).view(-1)
 
-# (batch, voices, time, channels)
+        return signal
+
+
+class Silence(nn.Module):
+    def __init__(self, samples_per_frame):
+        super().__init__()
+        self.samples_per_frame = samples_per_frame
+        self.register_buffer('silence', torch.zeros(samples_per_frame).fill_(1e-8))
+    
+    def forward(self, x, max_iter=10):
+        samples = torch.cat([self.silence for _ in range(max_iter)], dim=0)
+        return samples
+
 
 
 class Conductor(nn.Module):
-    def __init__(self, layers, channels, voices):
+    def __init__(self, layers, channels, voices, samples_per_frame, total_frames):
         super().__init__()
         self.layers = layers
         self.channels = channels
         self.voices = voices
+        self.samples_per_frame = samples_per_frame
+        self.total_frames = total_frames
 
         instruments = \
-            [RecurrentSynth(layers, channels) for _ in range(self.voices)] \
-            + [(RecurrentSynth(layers, channels, is_silent=True))]
+            [RecurrentSynth(layers, channels, samples_per_frame) for _ in range(self.voices)] \
+            + [Silence(self.samples_per_frame)]
         
-        self.instruments = nn.ParameterList(instruments)
+        self.instruments = nn.ModuleList(instruments)
 
         self.net = LinearOutputStack(channels, layers)
         self.router = LinearOutputStack(
             channels, layers, out_channels=self.voices + 1)
-        
-        # self.osc = OscillatorBank(
-        #     input_channels=128,
-        #     n_osc=128,
-        #     n_audio_samples=n_samples,
-        #     activation=torch.sigmoid,
-        #     amp_activation=torch.abs,
-        #     return_params=True,
-        #     constrain=True,
-        #     log_frequency=False,
-        #     lowest_freq=40 / samplerate.nyquist,
-        #     sharpen=False,
-        #     compete=False)
 
-        # self.noise = NoiseModel(
-        #     input_channels=128,
-        #     input_size=128,
-        #     n_noise_frames=512,
-        #     n_audio_samples=n_samples,
-        #     channels=128,
-        #     activation=lambda x: x,
-        #     squared=False,
-        #     mask_after=1,
-        #     return_params=True)
+        n_rooms = 8
+        self.to_rooms = LinearOutputStack(channels, layers, out_channels=n_rooms)
+        self.to_mix = LinearOutputStack(channels, layers, out_channels=1)
+        
+        self.verb = NeuralReverb(self.total_frames * self.samples_per_frame, n_rooms)
+
+        self.apply(init_weights)
+        
 
     def forward(self, x):
         batch, time, channels = x.shape
         if batch != 1:
             raise NotImplementedError('Batch mode not implemented')
         
+
+        z = torch.mean(x, dim=1)
+        rooms = torch.softmax(self.to_rooms(z), dim=-1)
+        mix = torch.sigmoid(self.to_mix(z))
+
+
+        total_samples = self.samples_per_frame * self.total_frames
+
+        out_samples = torch.zeros(total_samples)
+
         x = x.view(time, channels)
 
         x = self.net(x)
 
         routes = self.router(x)
+        # get probabilities for each instrument at each time step
         routes = torch.softmax(routes, dim=-1)
-        indices = torch.argmax(routes, dim=1)
+        # get the winning instrument at each time step
+        indices = torch.argmax(routes, dim=-1)
 
         for i in range(time):
-            r = routes[indices[i]]
+            # get the winning instrument index a this time step
+            index = indices[i].item()
+            # get the factor used to scale the output from the instrument
+            r = routes[i, index]
+            inst = self.instruments[index]
 
-            if r == self.voices:
-                continue
+            # run the instrument and scale it by the probability
+            inst_output = inst.forward(x[i], time - i) * r
+            
+            if len(inst_output.shape) == 1:
+                start_sample = i * self.samples_per_frame
+                end_sample = start_sample + inst_output.shape[0]
 
-            inst = self.instruments[r]
-            params, env = inst.forward(x[i], time - i)
-            # TODO: Pass params to synthesizer and apply envelope =)-hi
+                if end_sample >= total_samples:
+                    slce = out_samples[start_sample:end_sample]
+                    size = slce.shape[0]
+                    out_samples[start_sample:end_sample] = out_samples[start_sample:end_sample] + inst_output[:size]
+                else:
+                    out_samples[start_sample:end_sample] = out_samples[start_sample:end_sample] + inst_output
+                
+        
+        wet = self.verb(out_samples, rooms)
+        dry = out_samples
+
+        out_samples = (dry * mix) + (wet * (1 - mix))
+        return out_samples
+            
--- scratch2.py
+++ scratch2.py
@@ -1,21 +1,68 @@
+from enum import auto
 import torch
 import zounds
 import numpy as np
+from data.audiostream import audio_stream
 from modules.normal_pdf import pdf
-from modules.recurrent import RecurrentSynth
+from modules.recurrent import RecurrentSynth, Conductor
+from torch import nn
+from torch.nn import functional as F
 
+from train.optim import optimizer
+from util import playable
+from torch import autograd
+
+
+
+sr = zounds.SR22050()
+
+n_samples = 2**15
+samples_per_frame = 256
+frames = n_samples // samples_per_frame
+channels = 128
+voices = 8
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.p = nn.Parameter(torch.zeros(1, frames, channels).normal_(0, 0.05))
+        self.con = Conductor(
+            layers=3, 
+            channels=channels, 
+            voices=voices, 
+            samples_per_frame=samples_per_frame, 
+            total_frames=frames)
+    
+    def forward(self, _):
+        return self.con(self.p)
+
+
+model = Model()
+optim = optimizer(model, lr=1e-3)
 
 if __name__ == '__main__':
 
     app = zounds.ZoundsApp(globals=globals(), locals=locals())
     app.start_in_thread(9999)
 
-    rec = RecurrentSynth(3, 128)
-    start = torch.zeros(1, 128).normal_(0, 1)
 
-    hidden, gate = rec.forward(start, max_iter=20)
-    hidden = hidden.data.cpu().numpy().squeeze().T
-    gate = gate.data.cpu().numpy().squeeze()
-    print(hidden.shape, gate.shape)
+    stream = audio_stream(1, n_samples, overfit=True, normalize=True, as_torch=True)
+    batch = next(stream).view(1, n_samples)
+
+    def real():
+        return playable(batch, sr)
+    
+    def fake():
+        return playable(recon, sr)
+
+    with autograd.detect_anomaly():
+        while True:
+            optim.zero_grad()
+            recon = model.forward(None).view(1, n_samples)
+            loss = torch.abs(batch - recon).sum()
+            loss.backward()
+            optim.step()
+            print(loss.item())
+
 
     input('Waiting...')
