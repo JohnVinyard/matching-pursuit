5623ec17e248dd2ac02858e94619a020210b888d
John Vinyard <johnwho@gmail.com>
2022-06-16 16:43:39 -0500

    Try cutting k-means out entirely

--- experiments/e_2022_6_16/experiment.py
+++ experiments/e_2022_6_16/experiment.py
@@ -1,60 +1,210 @@
+from imp import init_builtin
 import torch
-from modules.overfitraw import OverfitRawAudio
-from modules.phase import morlet_filter_bank
-from modules.scattering import batch_fft_convolve, scattering_transform
+from modules.ddsp import NoiseModel, OscillatorBank
+from modules.linear import LinearOutputStack
+from modules.pif import AuditoryImage
+from modules.reverb import NeuralReverb
 from train.optim import optimizer
-from util import playable, readme
+from util import device, playable, readme
 import zounds
+from torch.nn import functional as F
+from torch import nn
+
+from util.weight_init import make_initializer
 
 samplerate = zounds.SR22050()
 band = zounds.FrequencyBand(20, samplerate.nyquist)
-scale = zounds.MelScale(band, 64)
-from torch.nn import functional as F
+scale = zounds.MelScale(band, 128)
+n_samples = 2**14
 
-bank = torch.from_numpy(morlet_filter_bank(samplerate, 512, scale, 0.1).real)
+fb = zounds.learn.FilterBank(samplerate, 512, scale, 0.1).to(device)
+aim = AuditoryImage(512, 64, do_windowing=True, check_cola=True).to(device)
 
-n_samples = 2**15
+n_clusters = 512
+
+model_dim = 128
+n_frames = 64
+n_noise_frames = 512
+n_rooms = 8
 
 
-model = OverfitRawAudio((1, n_samples), std=0.1)
-optim = optimizer(model, lr=1e-2)
+init_weights = make_initializer(0.1)
+
+class DilatedBlock(nn.Module):
+    def __init__(self, channels, dilation):
+        super().__init__()
+        self.dilated = nn.Conv1d(
+            channels, channels, 3, padding=dilation, dilation=dilation)
+        self.conv = nn.Conv1d(channels, channels, 1, 1, 0)
+
+    def forward(self, x):
+        orig = x
+        x = self.dilated(x)
+        x = self.conv(x)
+        x = F.leaky_relu(x + orig, 0.2)
+        return x
+
+
+class AudioModel(nn.Module):
+    def __init__(self, n_samples):
+        super().__init__()
+        self.n_samples = n_samples
+
+        self.osc = OscillatorBank(
+            model_dim, 
+            len(scale), 
+            n_samples, 
+            constrain=False, 
+            lowest_freq=40 / samplerate.nyquist,
+            amp_activation=lambda x: x ** 2,
+            complex_valued=True)
+        
+        self.noise = NoiseModel(
+            model_dim,
+            n_frames,
+            n_noise_frames,
+            n_samples,
+            model_dim,
+            squared=True,
+            mask_after=1)
+        
+        self.verb = NeuralReverb(n_samples, n_rooms)
+
+        self.to_rooms = LinearOutputStack(model_dim, 3, out_channels=n_rooms)
+        self.to_mix = LinearOutputStack(model_dim, 3, out_channels=1)
+
+    
+    def forward(self, x):
+        x = x.view(-1, model_dim, n_frames)
+
+        agg = x.mean(dim=-1)
+        room = self.to_rooms(agg)
+        mix = torch.sigmoid(self.to_mix(agg)).view(-1, 1, 1)
+
+        harm = self.osc.forward(x)
+        noise = self.noise(x)
+
+        dry = harm + noise
+        wet = self.verb(dry, room)
+        signal = (dry * mix) + (wet * (1 - mix))
+        return signal
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # self.encode = nn.Sequential(
+        #     nn.Conv2d(64, 64, 7, 4, 3),
+        #     nn.LeakyReLU(0.2),
+        #     nn.Conv2d(64, 64, 7, 4, 3),
+        #     nn.LeakyReLU(0.2),
+        # )
+
+        self.reduce = LinearOutputStack(model_dim, 3, out_channels=8, in_channels=257)
+        self.encode = nn.Sequential(
+            nn.Conv1d(8 * model_dim, model_dim, 1, 1, 0),
+            DilatedBlock(128, 1),
+            DilatedBlock(128, 3),
+            DilatedBlock(128, 9),
+            DilatedBlock(128, 1),
+        )
+
+        self.quantize = nn.Conv1d(model_dim, n_clusters, 1, 1, 0)
+
+        self.decode = nn.Sequential(
+            nn.Conv1d(n_clusters, 128, 1, 1, 0),
+            DilatedBlock(128, 1),
+            DilatedBlock(128, 3),
+            DilatedBlock(128, 9),
+            DilatedBlock(128, 1),
+        )
+
+        self.to_envelope = nn.Conv1d(model_dim, 1, 7, 1, 3)
+
+        self.audio = AudioModel(n_samples)
+
+        self.apply(init_weights)
+
+    def forward(self, x):
+        x = x.view(-1, 1, n_samples)
+        x = fb.forward(x, normalize=False)
+        x = aim.forward(x)
+
+
+        x = self.reduce(x)
+
+        x = x.permute(0, 3, 1, 2).reshape(-1, 8 * model_dim, 64)
+        x = self.encode(x)
+
+
+        # x = x.permute(0, 2, 1, 3)
+        # x = self.encode(x)
+        # x = x.view(-1, 64, 8 * 17).permute(0, 2, 1)
+
+        x = self.quantize(x)
+
+        x = x.permute(0, 2, 1).reshape(-1, n_clusters)
+        indices = torch.argmax(x, dim=-1)
+
+        n = torch.zeros_like(x)
+        n[torch.arange(x.shape[0]), indices] = x[torch.arange(x.shape[0]), indices]
+        n = n / (n.max(dim=-1, keepdim=True)[0] + 1e-12)
+
+
+        n = n.view(-1, 64, n_clusters).permute(0, 2, 1)
+
+        x = self.decode(n)
+
+        env = torch.abs(self.to_envelope(x))
+        env = F.interpolate(env, size=n_samples)
+        signal = self.audio(x)
+        signal = signal * env
+        return n, signal
+    
+ae = AutoEncoder().to(device)
+optim = optimizer(ae, lr=1e-4)
+
+
+def train_ae(batch):
+    optim.zero_grad()
+    encoded, decoded = ae.forward(batch)
+
+    f = aim.forward(decoded)
+    r = aim.forward(batch)
+
+    loss = F.mse_loss(f, r)
+    loss.backward()
+
+    optim.step()
+    return loss, encoded, decoded
+
 
 @readme
 class ScatteringTransformTest(object):
     def __init__(self, stream):
         super().__init__()
         self.stream = stream
-        self.c1 = None
-        self.c2 = None
-        self.audio = None
         self.real = None
 
-    def view1(self):
-        return self.c1.data.cpu().numpy()[0].T
+        self.encoded = None
+        self.decoded = None
 
-    def view2(self):
-        return self.c2.data.cpu().numpy()[0].T
-    
     def listen(self):
-        return playable(self.audio, samplerate)
+        return playable(self.decoded, samplerate)
     
+    def code(self):
+        return self.encoded.data.cpu().numpy()[0]
+
     def r(self):
         return playable(self.real, samplerate)
 
     def run(self):
-        for item in self.stream:
-            self.c1, self.c2 = scattering_transform(item, bank)
+        for i, item in enumerate(self.stream):
+            item = item.view(-1, 1, n_samples)
             self.real = item
 
-            while True:
-                optim.zero_grad()
+            loss, self.encoded, self.decoded = train_ae(item)
 
-                self.audio = model.forward(None)
-                c1, c2 = scattering_transform(self.audio, bank)
-
-
-                loss = F.mse_loss(c1, self.c1) + F.mse_loss(c2, self.c2) * 100
-                loss.backward()
-                optim.step()
+            if i % 10 == 0:
                 print(loss.item())
-
--- modules/scattering.py
+++ modules/scattering.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
 from torch.nn import functional as F
+from torch.jit._script import ScriptModule, script_method
+
+from fft_basis import morlet_filter_bank
 
 
 def batch_fft_convolve(signal, d):
@@ -13,37 +16,101 @@ def batch_fft_convolve(signal, d):
     px = F.pad(signal, (half_width, half_width))
     py = F.pad(d, (0, px.shape[1] - atom_size))
 
-
+    # signal
     fpx = torch.fft.rfft(px, dim=-1, norm='ortho')[:, None, ...]
 
+    # filter bank
     fpy = torch.fft.rfft(
         torch.flip(py, dims=(-1,)),
         dim=-1)[None, ...]
 
     c = fpx * fpy
 
-    new_size = (c.shape[-2] - 1) * 2
     c = torch.fft.irfft(c, dim=-1, norm='ortho')
 
     return c[..., :signal.shape[-1]]
 
 
 def scattering_transform(signal, d, window_size=512, step_size=256):
+    n_filters = d.shape[0]
+
     batch, samples = signal.shape
 
     s1 = torch.abs(batch_fft_convolve(signal, d))
     s1 = s1.view(batch, -1, samples)
-    pooled = F.avg_pool1d(s1, kernel_size=window_size, stride=1, padding=step_size)[..., :samples]
-    
-    c1 = F.avg_pool1d(pooled, kernel_size=step_size, stride=step_size, padding=step_size // 2)
+    pooled = F.avg_pool1d(s1, kernel_size=window_size,
+                          stride=1, padding=step_size)[..., :samples]
+
+    c1 = F.avg_pool1d(pooled, kernel_size=step_size,
+                      stride=step_size, padding=step_size // 2)
 
     s2 = s1 - pooled
 
     s2 = s2.view(-1, samples)
+
     s2 = torch.abs(batch_fft_convolve(s2, d))
     s2 = s2.view(batch, -1, samples)
-    c2 = F.avg_pool1d(s2, kernel_size=window_size, stride=step_size, padding=step_size)
+    c2 = F.avg_pool1d(s2, kernel_size=window_size,
+                      stride=step_size, padding=step_size)
 
     return c1, c2
 
 
+class ScatteringTransform(ScriptModule):
+    def __init__(self, signal_size, scale, samplerate, kernel_size=512):
+        super().__init__()
+        bank = torch.from_numpy(
+            morlet_filter_bank(samplerate, kernel_size, scale, 0.1).real)
+        py = F.pad(bank, (0, signal_size))
+        # filter bank
+        fpy = torch.fft.rfft(
+            torch.flip(py, dims=(-1,)),
+            dim=-1)[None, ...]
+        self.register_buffer('fpy', fpy)
+        self.kernel_size = kernel_size
+        self.signal_size = signal_size
+
+    @script_method
+    def _inner_forward(self, signal):
+        # signal_size = signal.shape[1]
+        atom_size = self.kernel_size
+        # diff = self.signal_size - self.kernel_size
+
+        half_width = atom_size // 2
+
+        px = F.pad(signal, (half_width, half_width))
+
+        # signal
+        fpx = torch.fft.rfft(px, dim=-1, norm='ortho')[:, None, ...]
+
+        c = fpx * self.fpy
+
+        c = torch.fft.irfft(c, dim=-1, norm='ortho')
+
+        return c[..., :signal.shape[-1]]
+
+    @script_method
+    def forward(self, signal):
+
+        batch, samples = signal.shape
+
+        s1 = torch.abs(self._inner_forward(signal))
+        s1 = s1.view(batch, -1, samples)
+        step_size = self.kernel_size // 2
+
+        pooled = F.avg_pool1d(s1, kernel_size=self.kernel_size,
+                              stride=1, padding=step_size)[..., :samples]
+
+        c1 = F.avg_pool1d(pooled, kernel_size=step_size,
+                          stride=step_size, padding=step_size // 2)
+
+        s2 = s1 - pooled
+
+        s2 = s2.view(-1, samples)
+
+        s2 = torch.abs(self._inner_forward(s2))
+        s2 = s2.view(batch, -1, samples)
+        c2 = F.avg_pool1d(s2, kernel_size=self.kernel_size,
+                          stride=step_size, padding=step_size)
+
+        return c1, c2
