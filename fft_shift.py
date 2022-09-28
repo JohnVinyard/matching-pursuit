807f436e8230f6339d98ba11748e6c1a14a97312
John Vinyard <johnwho@gmail.com>
2022-09-28 05:56:47 -0500

    more fiddling with fft shift

--- fft_shift.py
+++ fft_shift.py
@@ -11,6 +11,7 @@ from upsample import ConvUpsample
 from torch.nn import functional as F
 from util import make_initializer
 from modules.stft import stft
+from util import playable
 
 from torch.distributions.normal import Normal
 
@@ -19,70 +20,69 @@ n_samples = 2 ** 15
 
 init = make_initializer(0.05)
 
+
+def fft_shift(a, shift):
+    n_samples = a.shape[-1]
+    shift_samples = shift * n_samples
+    a = F.pad(a, (0, n_samples))
+    spec = torch.fft.rfft(a, dim=-1, norm='ortho')
+
+    n_coeffs = spec.shape[-1]
+    shift = (torch.arange(0, n_coeffs) * 2j * np.pi) / n_coeffs
+    shift = torch.exp(-shift * shift_samples)
+
+    spec = spec * shift
+
+    samples = torch.fft.irfft(spec, dim=-1, norm='ortho')
+    # samples = samples[..., :n_samples]
+    return samples
+
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.means = nn.Parameter(torch.zeros(2, 1).uniform_(0, 1))
-        # self.register_buffer('stds', torch.zeros(1).fill_(1 / n_samples))
-        # self.stds = nn.Parameter(torch.zeros(2, 1).uniform_(0, 0.01))
-
-        # self.latents = nn.Parameter(torch.zeros(2, 16).normal_(0, 1))
-
-        self.impulses = nn.Parameter(torch.zeros(2, n_samples).normal_(0, 1))
-
-        # self.gen = ConvUpsample(16, 16, 4, 8192, mode='learned', out_channels=1)
-        # self.apply(init)
-
-        # self.down = nn.Conv1d(257, 32, 1, 1, 0)
-        # self.up = nn.Conv1d(33, 32, 1, 1, 0)
-        # self.stack = DilatedStack(32, [1, 3, 9, 1])
-        # self.to_means = nn.Linear(32, 2)
-        # self.to_stds = nn.Linear(32, 2)
-        # self.apply(init)
+        self.shifts = nn.Parameter(torch.zeros(2, 1, 1).normal_(0, np.pi))
     
     def forward(self, x, gen_signals):
-
-        # x = stft(x, 512, 256, pad=True).permute(0, 2, 1)
-        # pos = pos_encoded(x.shape[0], 128, 16, device=x.device).permute(0, 2, 1).view(-1, 33, 128)
-        # pos = self.up(pos)
-        # x = self.down(x)
-        # x = x + pos
-        # x = self.stack(x)
-        # x, _ = torch.max(x, dim=-1)
-
-        # means = self.to_means(x).view(2, 1)
-        # stds = self.to_stds(x).view(2, 1)
-
-
-        # r = torch.linspace(0, 1, n_samples)
-        # dist = Normal(torch.sigmoid(means), 1e-12 + torch.sigmoid(stds))
-        # impulses = torch.exp(dist.log_prob(r[None, :])).view(2, 1, n_samples)
-        # mx, _ = torch.max(impulses, dim=-1, keepdim=True)
-        # impulses = impulses / (mx + 1e-12)
-
-        # impulses = F.gumbel_softmax(self.impulses, dim=-1, hard=False)
-        impulses = torch.softmax(self.impulses, dim=-1)
-        impulses = impulses.view(2, 1, n_samples)
-        
         gen_signals = gen_signals.view(2, 1, 8192)
         gen_signals = F.pad(gen_signals, (0, n_samples - 8192))
-
-        impulse_spec = torch.fft.rfft(impulses, norm='ortho', dim=-1)
-        gen_spec = torch.fft.rfft(gen_signals, norm='ortho', dim=-1)
-        spec = impulse_spec * gen_spec
-        inverse = torch.fft.irfft(spec, norm='ortho', dim=-1)
-
-        signal = torch.sum(inverse, dim=0).view(1, 1, n_samples)
-        signal = signal / (signal.max() + 1e-8)
-        return signal, impulses
+        signal = fft_shift(gen_signals, torch.sin(self.shifts))
+        signal = signal.sum(dim=0).view(1, 1, n_samples * 2)
+        return signal
 
 
 model = Model()
 optim = optimizer(model, lr=1e-4)
 
 
+def does_this_work():
+    attack = np.zeros(2**16)
+    attack[:64] = np.random.uniform(-1, 1, 64) * np.hamming(64)
+    print(attack.max())
+
+    t = np.zeros(2**16)
+    synth = zounds.SineSynthesizer(samplerate)
+    tone = synth.synthesize(samplerate.frequency * 8192, [220])
+    t[:8192] = tone * np.linspace(1, 0, 8192) ** 2
+    print(t.max())
+
+    loc = np.zeros(2**16)
+    loc[2**13] = 1
+    print(loc.max())
+
+    a = np.fft.rfft(attack, axis=-1, norm='ortho')
+    t = np.fft.rfft(t, axis=-1, norm='ortho')
+    l = np.fft.rfft(loc, axis=-1, norm='ortho')
+
+    spec = a * t * l
+    final = np.fft.irfft(spec, axis=-1, norm='ortho')
+    final = final[:2**15]
+    return zounds.AudioSamples(final, samplerate).pad_with_silence()
+    
+
 def train(batch, a, b):
     optim.zero_grad()
+
+    batch = F.pad(batch, (0, n_samples))
 
     a = a * np.hamming(8192)
     b = b * np.hamming(8192)
@@ -90,78 +90,101 @@ def train(batch, a, b):
     gen_signals = np.concatenate([a[None, ...], b[None, ...]], axis=0)
     gen_signals = torch.from_numpy(gen_signals).float()
 
-    x, impulses = model.forward(batch, gen_signals)
- 
-    # print(10 + 4096, 16444 + 4096)
-    # print(torch.argmax(impulses, dim=-1))
+    x = model.forward(batch, gen_signals)
 
-    spec = stft(batch.view(1, 1, n_samples), pad=True).view(128, 257)
-    fspec = stft(x.view(1, 1, n_samples), pad=True).view(128, 257)
+    spec = stft(batch.view(1, 1, -1), pad=True).view(-1, 257)
+    fspec = stft(x.view(1, 1, -1), pad=True).view(-1, 257)
 
-    diff = ((spec - fspec) ** 2).mean()
+    r_sim = torch.cdist(spec, spec)
+    f_sim = torch.cdist(fspec, fspec)
 
-    # rsim = spec @ spec.T
-    # fsim = fspec @ fspec.T
+    sim_loss = F.mse_loss(f_sim, r_sim)
 
-    # rsim = torch.cdist(spec, spec)
-    # fsim = torch.cdist(fspec, fspec.detach())
+    loss = ((spec - fspec) ** 2).mean() + sim_loss
 
-    # sim_loss = ((rsim - fsim) ** 2).mean()
+    spec = torch.fft.rfft(batch.view(1, 1, -1), dim=-1, norm='ortho')
+    fspec = torch.fft.rfft(x.view(1, 1, -1), dim=-1, norm='ortho')
+    spec = torch.cat([spec.real, spec.imag], dim=-1)
+    fspec = torch.cat([fspec.real, fspec.imag], dim=-1)
+    full_loss = F.mse_loss(fspec, spec)
+    
+    loss = loss + full_loss
 
-    loss = diff
-
-    # spec = torch.fft.rfft(batch, dim=-1, norm='ortho')
-    # fspec = torch.fft.rfft(x, dim=-1, norm='ortho')
-
-    # mag_loss = F.mse_loss(fspec.real, spec.real)
-    # phase_loss = F.mse_loss(fspec.imag, spec.imag)
-
-    # loss = mag_loss + phase_loss
+    # loss = sim_loss
 
 
     loss.backward()
     optim.step()
     print(loss.item())
-    # print('===================================')
 
-    return x, impulses
+    return x
+
+'''
+def phase_shift(coeffs, samplerate, time_shift, axis=-1, frequency_band=None):
+    frequency_dim = coeffs.dimensions[axis]
+    if not isinstance(frequency_dim, FrequencyDimension):
+        raise ValueError(
+            'dimension {axis} of coeffs must be a FrequencyDimension instance, '
+            'but was {cls}'.format(axis=axis, cls=frequency_dim.__class__))
+
+    n_coeffs = coeffs.shape[axis]
+    shift_samples = int(time_shift / samplerate.frequency)
+    shift = (np.arange(0, n_coeffs) * 2j * np.pi) / n_coeffs
+    shift = np.exp(-shift * shift_samples)
+    shift = ArrayWithUnits(shift, [frequency_dim])
+
+    frequency_band = frequency_band or slice(None)
+    new_coeffs = coeffs.copy()
+
+    if coeffs.ndim == 1:
+        new_coeffs[frequency_band] *= shift[frequency_band]
+        return new_coeffs
+
+    slices = [slice(None) for _ in range(coeffs.ndim)]
+    slices[axis] = frequency_band
+    new_coeffs[tuple(slices)] *= shift[frequency_band]
+    return new_coeffs
+'''
+
+
 
 
 if __name__ == '__main__':
     app = zounds.ZoundsApp(locals=locals(), globals=globals())
     app.start_in_thread(8888)
 
-    synth = zounds.SineSynthesizer(samplerate)
+    x = does_this_work()
+
+    # synth = zounds.SineSynthesizer(samplerate)
+
+    # a = synth.synthesize(samplerate.duration * 8192, [440])
+    # b = synth.synthesize(samplerate.duration * 8192, [210])
+
+    # canvas = np.zeros(n_samples)
+    # start_a = 10
+    # start_b = 16444
+
+    # canvas[start_a: start_a + 8192] = a * np.hamming(8192)
+    # canvas[start_b: start_b + 8192] = b * np.hamming(8192)
 
 
-    a = synth.synthesize(samplerate.duration * 8192, [440])
-    b = synth.synthesize(samplerate.duration * 8192, [210])
+    # samples = zounds.AudioSamples(canvas, samplerate).pad_with_silence()
 
-    canvas = np.zeros(n_samples)
-    start_a = 10
-    start_b = 16444
+    # t_samples = torch.from_numpy(canvas).float()
 
-    canvas[start_a: start_a + 8192] = a * np.hamming(8192)
-    canvas[start_b: start_b + 8192] = b * np.hamming(8192)
+    # # rng = torch.linspace(0, 1, 128)
+    # # means = torch.zeros(16).uniform_(0, 1)
+    # # stds = torch.zeros(16).uniform_(0, 0.1)
 
-    samples = zounds.AudioSamples(canvas, samplerate).pad_with_silence()
+    # # pdf_example = pdf(rng[None, :], means[:, None], stds[:, None])
+    # # mx, _ = torch.max(pdf_example, dim=-1, keepdim=True)
+    # # pdf_example = pdf_example / (mx + 1e-8)
+    # # pdf_example = pdf_example.data.cpu().numpy()
 
-    t_samples = torch.from_numpy(canvas).float()
-
-    rng = torch.linspace(0, 1, 128)
-    means = torch.zeros(16).uniform_(0, 1)
-    stds = torch.zeros(16).uniform_(0, 0.1)
-
-    pdf_example = pdf(rng[None, :], means[:, None], stds[:, None])
-    mx, _ = torch.max(pdf_example, dim=-1, keepdim=True)
-    pdf_example = pdf_example / (mx + 1e-8)
-    pdf_example = pdf_example.data.cpu().numpy()
-
-    while True:
-        result, impulses = train(t_samples, a, b)
-        ii = impulses.data.cpu().numpy().squeeze()
-        r = result.data.cpu().numpy().squeeze()
-        rr = zounds.AudioSamples(r, samplerate).pad_with_silence()
+    # while True:
+    #     result = train(t_samples, a, b)
+    #     r = result.data.cpu().numpy().squeeze()
+    #     rr = zounds.AudioSamples(r, samplerate).pad_with_silence()
 
     input('waiting')
 
