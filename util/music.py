f1f2fe56b53e6d8d824c86969b27ce7b513adda1
John Vinyard <johnwho@gmail.com>
2022-10-08 08:58:53 -0500

    Make musical scale

---
+++ util/music.py
@@ -0,0 +1,28 @@
+# from matplotlib import pyplot as plt
+import numpy as np
+import zounds
+
+import matplotlib
+matplotlib.use('qt5agg', force=True)
+import matplotlib.pyplot as plt
+
+def midi_to_hz(n: int):
+    return 440 * (2 ** ((n - 69) / 12))
+
+
+def musical_scale(start_midi=1, stop_midi=129):
+    x = midi_to_hz(np.arange(start_midi, stop_midi))
+    for freq in x:
+        yield zounds.FrequencyBand(freq - 1, freq + 1)
+
+
+if __name__ == '__main__':
+    x = midi_to_hz(np.arange(1, 128))
+
+    band = zounds.FrequencyBand(20, zounds.SR22050().nyquist)
+    scale = zounds.MelScale(band, 128)
+
+    plt.plot(x, label='musical')
+    plt.plot(list(scale.center_frequencies), label='mel')
+    plt.legend()
+    plt.show()
