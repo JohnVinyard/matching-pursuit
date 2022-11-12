dcedb825f0b319303d571b2aa24633dad5d4e822
John Vinyard <john.vinyard@gmail.com>
2022-11-12 15:50:44 -0600

    Tweaking things.  Perhaps the renderer network isn't necessary

--- experiments/e_2022_11_9/experiment.py
+++ experiments/e_2022_11_9/experiment.py
@@ -9,10 +9,11 @@ from modules.linear import LinearOutputStack
 from modules.normalization import ExampleNorm
 from modules.phase import AudioCodec, MelScale
 from modules.pos_encode import pos_encoded
+from modules.sparse import VectorwiseSparsity
 from modules.stft import stft
 from train.optim import optimizer
-from upsample import ConvUpsample
-from util import playable
+from upsample import ConvUpsample, PosEncodedUpsample
+from util import device, playable
 from util.readmedocs import readme
 import numpy as np
 from torch.nn import functional as F
@@ -24,7 +25,7 @@ from util.weight_init import make_initializer
 exp = Experiment(
     samplerate=zounds.SR22050(),
     n_samples=2**15,
-    weight_init=0.05,
+    weight_init=0.1,
     model_dim=128,
     kernel_size=512)
 
@@ -54,7 +55,7 @@ class Window(nn.Module):
 
     def forward(self, means, stds):
         dist = Normal(self.mn + (means * self.scale), self.epsilon + stds)
-        rng = torch.linspace(0, 1, self.n_samples)[None, None, :]
+        rng = torch.linspace(0, 1, self.n_samples, device=means.device)[None, None, :]
         windows = torch.exp(dist.log_prob(rng))
         return windows
 
@@ -109,7 +110,7 @@ class BandLimitedNoise(nn.Module):
 
     def forward(self, center_frequencies, bandwidths):
         windows = self.window.forward(center_frequencies, bandwidths)
-        n = torch.zeros(1, 1, self.n_samples).uniform_(-1, 1)
+        n = torch.zeros(1, 1, self.n_samples, device=center_frequencies.device).uniform_(-1, 1)
         noise_spec = torch.fft.rfft(n, dim=-1, norm='ortho')
         # filter noise in the frequency domain
         filtered = noise_spec * windows
@@ -154,7 +155,7 @@ class Renderer(nn.Module):
         specs = torch.mean(specs, dim=1)
         specs = specs.permute(0, 2, 1)
         # we're producing magnitudes, so positive values only
-        specs = torch.relu(specs)
+        specs = specs ** 2
         return specs
 
 
@@ -172,7 +173,7 @@ class Model(nn.Module):
         self.resonance = Resonance(
             n_samples, 
             samples_per_frame=samples_per_frame, 
-            factors=torch.arange(1, n_harmonics + 1),
+            factors=torch.arange(1, n_harmonics + 1, device=device),
             samplerate=int(exp.samplerate),
             min_freq_hz=min_center_freq,
             max_freq_hz=max_center_freq)
@@ -181,38 +182,52 @@ class Model(nn.Module):
             Config.impulse_response_path(), exp.samplerate, exp.n_samples)
         self.n_rooms = self.verb.n_rooms
         
-        self.ln = nn.Linear(10, 11)
-
         self.context = DilatedStack(exp.model_dim, [1, 3, 9, 27, 1])
         self.reduce = nn.Conv1d(exp.model_dim + 33, exp.model_dim, 1, 1, 0)
         self.norm = ExampleNorm()
 
+        self.sparse = VectorwiseSparsity(
+            exp.model_dim, keep=n_events, channels_last=False, dense=False, normalize=True)
+
         self.to_mix = LinearOutputStack(exp.model_dim, 2, out_channels=1)
         self.to_room = LinearOutputStack(exp.model_dim, 2, out_channels=self.n_rooms)
 
+        self.to_event_params = nn.Sequential(
+            # PosEncodedUpsample(
+            #     exp.model_dim, 
+            #     exp.model_dim, 
+            #     size=n_events, 
+            #     out_channels=params_per_event, 
+            #     layers=4)
+
+            LinearOutputStack(exp.model_dim, 2, out_channels=params_per_event)
+        )
+
         # (n_harmonics + 6) * n_events
 
-        self.to_f0 = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
-        self.to_harmonics = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events * n_harmonics)
+        # self.to_f0 = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
+        # self.to_harmonics = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events * n_harmonics)
 
-        self.to_amplitudes = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
+        # self.to_amplitudes = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
 
-        self.to_freq_means = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
-        self.to_freq_stds = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
+        # self.to_freq_means = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
+        # self.to_freq_stds = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
 
-        self.to_time_means = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
-        self.to_time_stds = LinearOutputStack(
-            exp.model_dim, 2, out_channels=n_events)
+        # self.to_time_means = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
+        # self.to_time_stds = LinearOutputStack(
+        #     exp.model_dim, 2, out_channels=n_events)
 
         self.apply(init_weights)
 
-    def forward(self, x):
+    def forward(self, x, noise_mix=0):
+
+        # encode 
         batch = x.shape[0]
         target = x = x.view(-1, 1, exp.n_samples)
         x = exp.fb.forward(x, normalize=False)
@@ -226,31 +241,67 @@ class Model(nn.Module):
         x = self.context(x)
         x = self.norm(x)
 
-        x, _ = torch.max(x, dim=-1)
+        x, indices = self.sparse(x)
+        
+        # x, _ = torch.max(x, dim=-1)
 
-        mx = torch.sigmoid(self.to_mix(x)).view(batch, 1, 1)
-        rooms = torch.softmax(self.to_room(x), dim=-1)
+        verb_params, _ = torch.max(x, dim=1)
+        
 
-        f0 = torch.sigmoid(self.to_f0.forward(x)).view(
-            batch, self.n_events, 1)
-        res = torch.sigmoid(self.to_harmonics(x)).view(
-            batch, self.n_events, self.n_harmonics)
+        '''
+        TODO: 
+            - Expand from (batch, model_dim) to (batch, n_events, n_event_params)
+            - optionally apply noise at this point
+            - apply sigmoid and transform to event params
+        '''
 
-        freq_means = torch.sigmoid(self.to_freq_means(x)).view(
-            batch, self.n_events, 1)
-        freq_stds = torch.sigmoid(self.to_freq_stds(x)).view(
-            batch, self.n_events, 1) * 0.1
+        # expand to params
+        mx = torch.sigmoid(self.to_mix(verb_params)).view(batch, 1, 1)
+        rooms = torch.softmax(self.to_room(verb_params), dim=-1)
 
-        time_means = torch.sigmoid(self.to_time_means(x)).view(
-            batch, self.n_events, 1)
-        time_stds = torch.sigmoid(self.to_time_stds(x)).view(
-            batch, self.n_events, 1) * 0.1
+        # f0 = torch.sigmoid(self.to_f0.forward(x)).view(
+        #     batch, self.n_events, 1)
+        # res = torch.sigmoid(self.to_harmonics(x)).view(
+        #     batch, self.n_events, self.n_harmonics)
 
-        amps = torch.sigmoid(self.to_amplitudes(x)).view(
-            batch, self.n_events, 1)
+        # freq_means = torch.sigmoid(self.to_freq_means(x)).view(
+        #     batch, self.n_events, 1)
+        # freq_stds = torch.sigmoid(self.to_freq_stds(x)).view(
+        #     batch, self.n_events, 1) * 0.1
 
-        event_params = torch.cat(
-            [f0, res, freq_means, freq_stds, time_means, time_stds, amps], dim=-1)
+        # time_means = torch.sigmoid(self.to_time_means(x)).view(
+        #     batch, self.n_events, 1)
+        # time_stds = torch.sigmoid(self.to_time_stds(x)).view(
+        #     batch, self.n_events, 1) * 0.1
+
+        # amps = torch.sigmoid(self.to_amplitudes(x)).view(
+        #     batch, self.n_events, 1)
+
+        # # (n_harmonics + 6) * n_events
+        # event_params = torch.cat(
+        #     [f0, res, freq_means, freq_stds, time_means, time_stds, amps], dim=-1)
+
+        event_params = self.to_event_params.forward(x).view(-1, n_events, params_per_event)
+
+        if noise_mix == 0:
+            event_params = torch.sigmoid(event_params)
+        else:
+            noise = torch.zeros_like(event_params, device=x.device).uniform_(0, 1)
+            actual_amt = 1 - noise_mix
+            event_params = (noise * noise_mix) + (actual_amt * torch.sigmoid(event_params))
+
+        
+        res_baseline = 0.999
+        res_span = 1 - res_baseline
+
+        freq_means = event_params[:, :, 0].view(batch, self.n_events, 1)
+        freq_stds = event_params[:, :, 1].view(batch, self.n_events, 1) * 0.1
+        time_means = event_params[:, :, 2].view(batch, self.n_events, 1)
+        time_stds = event_params[:, :, 3].view(batch, self.n_events, 1) * 0.1
+        f0 = event_params[:, :, 4].view(batch, self.n_events, 1) ** 2
+        amps = event_params[:, :, 5].view(batch, self.n_events, 1)
+        res = (event_params[:, :, 6:].view(batch, self.n_events, self.n_harmonics) * res_span) + res_baseline
+
 
         # gradients do not flow through the following operations
         windows = self.noise.forward(freq_means, freq_stds)
@@ -271,27 +322,34 @@ class Model(nn.Module):
 
         located = (dry * (1 - mx)) + (wet * mx)
 
-        return located, x, event_params
+        return located, verb_params, event_params
 
 model = Model(
     exp.n_samples, 
     n_events, 
     n_harmonics, 
-    samples_per_frame)
-optim = optimizer(model, lr=1e-3)
+    samples_per_frame).to(device)
+optim = optimizer(model, lr=1e-4)
 
 render = Renderer(
     exp.model_dim, 
     n_frames=128, 
     n_freq_bins=256, 
-    n_events=n_events)
-render_optim = optimizer(render, lr=1e-3)
+    n_events=n_events).to(device)
+render_optim = optimizer(render, lr=1e-4)
 
-def train_renderer(batch):
-    optim.zero_grad()
+def iteration_to_noise_mix(iteration):
+    # value = 1 - (iteration * 1e-5)
+    # return max(value, 0)
+    return 0
+
+def train_renderer(batch, iteration):
+    render_optim.zero_grad()
+
+    noise_mix = iteration_to_noise_mix(iteration)
 
     with torch.no_grad():
-        recon, latent, params = model.forward(batch)
+        recon, latent, params = model.forward(batch, noise_mix=noise_mix)
         params = params.view(-1, params_per_event)
     
     rendered = render.forward(params, latent)
@@ -304,12 +362,31 @@ def train_renderer(batch):
     render_optim.step()
     return loss, rendered, actual
 
-def train(batch):
+def train(batch, iteration):
     optim.zero_grad()
     recon, latent, params = model.forward(batch)
     real_spec = codec.to_frequency_domain(batch.view(-1, exp.n_samples))[..., 0]
     pred_spec = render.forward(params.view(-1, params_per_event), latent)
-    loss = F.mse_loss(pred_spec, real_spec)
+    loss = F.mse_loss(pred_spec, real_spec) + torch.abs(batch.std() - recon.std())
+    loss.backward()
+    optim.step()
+    return recon, latent, loss
+
+
+def train_direct(batch, iteration):
+    optim.zero_grad()
+    recon, latent, params = model.forward(batch)
+
+    # real_spec = codec.to_frequency_domain(batch.view(-1, exp.n_samples))[..., 0]
+    # pred_spec = codec.to_frequency_domain(recon.view(-1, exp.n_samples))[..., 0]
+
+    # real_spec = stft(batch, 512, 256, pad=True)
+    # pred_spec = stft(recon, 512, 256, pad=True)
+
+    real_spec = exp.perceptual_feature(batch)
+    fake_spec = exp.perceptual_feature(recon)
+
+    loss = F.mse_loss(fake_spec, real_spec)
     loss.backward()
     optim.step()
     return recon, latent, loss
@@ -325,8 +402,8 @@ class ResonantAtomsExperiment(object):
         self.rendered = None
         self.actual = None
 
-    def actual_spec(self):
-        return self.actual.data.cpu().numpy()[0]
+    # def actual_spec(self):
+        # return self.actual.data.cpu().numpy()[0]
 
     def real_spec(self):
         sp = codec.to_frequency_domain(self.real.view(-1, exp.n_samples))
@@ -334,12 +411,16 @@ class ResonantAtomsExperiment(object):
 
     def orig(self):
         return playable(self.real, exp.samplerate)
+    
+    def fake_spec(self):
+        sp = codec.to_frequency_domain(self.win[0, 0].view(-1, exp.n_samples))
+        return sp.data.cpu().numpy()[0, ..., 0]
 
     def listen(self):
         return playable(self.win[0, 0], exp.samplerate)
 
-    def fake_spec(self):
-        return self.rendered.data.cpu().numpy()[0]
+    # def fake_spec(self):
+        # return self.rendered.data.cpu().numpy()[0]
     
     def z(self):
         return self.latent.data.cpu().numpy().squeeze()
@@ -348,15 +429,19 @@ class ResonantAtomsExperiment(object):
         for i, item in enumerate(self.stream):
             item = item.view(-1, 1, exp.n_samples)
             self.real = item
+
+            w, latent, loss = train_direct(item, i)
+            self.win = w
+            self.latent = latent
+            print(i, 'D', loss.item())
         
-            if i % 2 == 0:
-                w, latent, loss = train(item)
-                self.win = w
-                self.latent = latent
-                print('G', loss.item())
-            else:
-                loss, rendered, actual = train_renderer(item)
-                self.rendered = rendered
-                self.actual = actual
-                print('R', loss.item())
+            # w, latent, loss = train(item, i)
+            # self.win = w
+            # self.latent = latent
+            # print(i, 'G', loss.item())
+
+            # loss, rendered, actual = train_renderer(item, i)
+            # self.rendered = rendered
+            # self.actual = actual
+            # print(i, 'R', loss.item())
 
