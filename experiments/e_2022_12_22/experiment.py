12c989015e61868e7391798d87e1baa8f9157898
John Vinyard <johnwho@gmail.com>
2022-12-23 11:49:39 -0600

    Getting closer by using group delay.  Fix bug that limited location

--- experiments/e_2022_12_22/experiment.py
+++ experiments/e_2022_12_22/experiment.py
@@ -40,7 +40,33 @@ exp = Experiment(
     model_dim=128,
     kernel_size=512)
 
-total_params = 1 + 1 + 1 + 257 + 16
+
+mean_slice = slice(0, 1)
+std_slice = slice(1, 2)
+amp_slice = slice(2, 3)
+mag_slice = slice(3, 260)
+phase_slice = slice(260, 260 + 257)
+noise_coeff_slice = slice(260 + 257, 260 + 257 + 16)
+
+
+def unpack(x):
+    means = (x[..., mean_slice] * 2) - 1
+    stds = x[..., std_slice] * 0.1
+    amps = x[..., amp_slice]
+
+    mag = min_resonance + (res_span * x[..., mag_slice])
+
+    # TODO: use the group-delay here, to simplify this
+    phase = (x[..., phase_slice] * np.pi * 2) - np.pi
+
+    noise_coeff = x[..., noise_coeff_slice]
+
+    return means, stds, amps, mag, phase, noise_coeff
+
+
+total_params = 1 + 1 + 1 + 257 + 257 + 16
+
+
 n_events = 16
 total_coeffs = exp.n_samples // 2 + 1
 window_size = 512
@@ -55,15 +81,40 @@ res_span = 1 - min_resonance
 def unit_activation(x):
     # return torch.sigmoid(x)
     return torch.clamp(x, 0, 1)
+    # return (torch.sin(x) + 1) * 0.5
 
 
 def to_mag_and_phase(x):
-    mag = torch.abs(x)
-    phase = torch.angle(x)
+
+    r, i = x.real, x.imag
+
+    paired = torch.cat([r[..., None], i[..., None]], dim=-1)
+
+    reference = torch.zeros_like(paired)
+    reference[..., 1] = 1
+
+    norm = torch.norm(paired, dim=-1) + 1e-8
+    ref_norm = torch.norm(reference, dim=-1)
+
+    freqs = torch.fft.rfftfreq(window_size) * 2 * np.pi
+    # normalized angle between vectors
+    phase = ((paired * reference).sum(dim=-1) / (norm * ref_norm))
+    phase = phase * freqs
+    # phase = (phase * np.pi) + freqs
+
+    mag = norm
+    # phase = torch.angle(x)
+    # print(phase)
     return mag, phase
 
 
 def to_complex(real, imag):
+    # tf = torch.complex(
+    #     real * torch.cos(imag),
+    #     real * torch.sin(imag)
+    # )
+    # return tf
+
     spec = real * torch.exp(1j * imag)
     return spec
 
@@ -77,10 +128,11 @@ def localized_noise(means, stds, spec_shape, n_samples, device):
     # create the gaussian windows
     rng = torch.arange(0, n_samples, device=device)
     dist = Normal(
-        # torch.clamp(means * n_samples, 0, 1), 
-        0,
+        torch.clamp(means * n_samples, -(n_samples // 2), n_samples * 1.5),
+        # 0,
         torch.clamp((1e-8 + stds) * n_samples, 0, n_samples - 1))
-    probs = torch.exp(dist.log_prob(rng[None, ...])).view(-1, n_events, n_samples)
+    probs = torch.exp(dist.log_prob(
+        rng[None, ...])).view(-1, n_events, n_samples)
     probs = max_norm(probs)
 
     # create white noise
@@ -99,11 +151,11 @@ def localized_noise(means, stds, spec_shape, n_samples, device):
     # using the gaussian windows
     noise = probs * noise
     noise = noise.view(-1, n_events, exp.n_samples)
-    noise = fft_shift(noise, means)[..., :exp.n_samples]
+    # noise = fft_shift(noise, means)[..., :exp.n_samples]
     return noise
 
 
-def resonance(noise_atoms, resonance):
+def resonance(noise_atoms, resonance, phase):
 
     noise_atoms = F.pad(noise_atoms, (0, step_size))
     windowed = \
@@ -111,24 +163,37 @@ def resonance(noise_atoms, resonance):
         * torch.hamming_window(window_size)[None, None, None, :]
     spec = torch.fft.rfft(windowed, dim=-1, norm='ortho')
 
-    res_specs = []
+    res_mags = []
+    res_phases = []
 
-    m = spec
+    mags, phases = to_mag_and_phase(spec)
+
 
     for i in range(n_frames):
-        # each successive frame should be impulse + (impulse * resonance) + (impulse-1 * resonance)
-        res = resonance 
-        imp = m[:, :, i, :]
+
+        curr_mag =     mags[:, :, i:i + 1, :]
+        curr_phase = phases[:, :, i:i + 1, :]
+
         if i == 0:
-            curr = imp + (imp * res)
+            res_mags.append(curr_mag)
+            res_phases.append(curr_phase)
         else:
-            prev = res_specs[-1]
-            curr =  imp + ((prev.view(-1, n_events, n_coeffs)) * res)
+            prev_mag = res_mags[i - 1]
+            prev_phase = res_phases[i - 1]
+
+            m = curr_mag + (resonance[:, :, None, :] * prev_mag)
+            p = curr_phase + (phase[:, :, None, :] + prev_phase)
+
+
+            res_mags.append(m)
+            res_phases.append(p)
         
-        curr = curr[:, :, None, :]
-        res_specs.append(curr)
-    
-    final = torch.cat(res_specs, dim=2)
+
+    mags = torch.cat(res_mags, dim=2)
+    phases = torch.cat(res_phases, dim=2)
+
+    final = to_complex(mags, phases)
+
     res = torch.fft.irfft(final, dim=-1, norm='ortho')
     res = overlap_add(res, apply_window=False)[..., :exp.n_samples]
     return res
@@ -142,14 +207,18 @@ class Atoms(nn.Module):
         x = unit_activation(x)
 
         # means can be before or after the audio frame
-        means = x[..., 0:1]
-        stds = x[..., 1:2]
-        res_mags = min_resonance + (res_span * x[..., 2:259])
-        spec_shape = x[..., 259:-1]
-        amps = x[..., -1:].view(-1, n_events, 1)
-        atoms = localized_noise(means, stds, spec_shape, exp.n_samples, device=device)
+        # means = x[..., 0:1]
+        # stds = x[..., 1:2]
+        # res_mags = min_resonance + (res_span * x[..., 2:259])
+        # spec_shape = x[..., 259:-1]
+        # amps = x[..., -1:].view(-1, n_events, 1)
+
+        means, stds, amps, res_mags, res_phases, spec_shape = unpack(x)
+
+        atoms = localized_noise(
+            means, stds, spec_shape, exp.n_samples, device=device)
         atoms = atoms * amps
-        res = resonance(atoms, res_mags)
+        res = resonance(atoms, res_mags, res_phases)
         res = res.view(-1, n_events, exp.n_samples)
 
         res = torch.sum(res, dim=1, keepdim=True)
@@ -160,14 +229,17 @@ class Atoms(nn.Module):
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.p = nn.Parameter(torch.zeros(1, n_events, total_params).uniform_(0, 1))
+        self.p = nn.Parameter(torch.zeros(
+            1, n_events, total_params).uniform_(0, 1))
         self.atoms = Atoms()
-    
+
     def forward(self, x):
         return self.atoms(self.p)
 
+
 model = Model().to(device)
 optim = optimizer(model, lr=1e-3)
+
 
 def train(batch):
     optim.zero_grad()
@@ -176,12 +248,12 @@ def train(batch):
 
     # real = stft(batch, 512, 256, pad=True)
     # fake = stft(recon, 512, 256, pad=True)
-
     # loss = F.mse_loss(fake, real)
 
     loss.backward()
     optim.step()
     return loss, recon
+
 
 @readme
 class ResonantMatchingPursuitExperiment(object):
