6310955c79de579c723cbfd5f0c43cdf7b77a799
John Vinyard <johnwho@gmail.com>
2023-02-19 07:49:02 -0600

    This works better with raw sample transfer functions

--- experiments/e_2023_2_16/experiment.py
+++ experiments/e_2023_2_16/experiment.py
@@ -1,4 +1,5 @@
 from config.experiment import Experiment
+from loss.least_squares import least_squares_disc_loss
 from modules.atoms import unit_norm
 from modules.ddsp import NoiseModel, noise_bank2
 from modules.dilated import DilatedStack
@@ -6,6 +7,7 @@ from modules.fft import fft_convolve
 from modules.linear import LinearOutputStack
 from modules.normalization import ExampleNorm, max_norm
 from modules.pos_encode import pos_encoded
+from modules.psychoacoustic import PsychoacousticFeature
 from modules.reverb import ReverbGenerator
 from modules.softmax import hard_softmax, sparse_softmax
 from modules.sparse import sparsify, sparsify_vectors
@@ -28,9 +30,11 @@ exp = Experiment(
     model_dim=128,
     kernel_size=512)
 
+
 def choice_softmax(x):
     # return sparse_softmax(x, normalize=False)
     return hard_softmax(x, invert=True)
+
 
 class DilatedBlock(nn.Module):
     def __init__(self, channels, dilation):
@@ -61,16 +65,13 @@ class ImpulseDictionary(nn.Module):
         self.n_coeffs = self.window_size // 2 + 1
         self.n_frames = self.n_samples // self.step_size
 
-        self.spectral_shapes = nn.Parameter(torch.zeros(self.n_atoms, self.n_coeffs, 1).uniform_(-1, 1).repeat(1, 1, self.n_frames))
-    
+        self.spectral_shapes = nn.Parameter(
+            torch.zeros(self.n_atoms, self.n_coeffs, 1).uniform_(-1, 1).repeat(1, 1, self.n_frames))
+
     def forward(self):
-        # normed = unit_norm(torch.sin(self.spectral_shapes), axis=1)
-        # normed = normed.repeat(1, 1, self.n_frames)
-        # with_envelope = normed * (self.envelopes ** 2)
-        # params = self.spectral_shapes
         as_samples = noise_bank2(self.spectral_shapes)
-        # as_samples = max_norm(as_samples, dim=-1)
         return as_samples.view(1, self.n_atoms, self.n_samples)
+
 
 class TransferFunctionDictionary(nn.Module):
     def __init__(self, n_atoms, n_samples, n_frequencies, n_frames):
@@ -81,28 +82,52 @@ class TransferFunctionDictionary(nn.Module):
         self.n_frames = n_frames
 
         self.t = nn.Parameter(torch.zeros(
-            1, self.n_atoms, self.n_samples).uniform_(-1, 1) * (torch.linspace(1, 0, self.n_samples) ** 20)[None, None, :])
+            1, self.n_atoms, self.n_samples).uniform_(-1, 1) * (torch.linspace(1, 0, self.n_samples) ** 15)[None, None, :])
 
-        self.osc = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
-        self.amp = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
-        self.decay = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
+        # self.osc = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
+        # self.amp = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
+        # self.decay = nn.Parameter(torch.zeros(self.n_atoms, self.n_frequencies).uniform_(0, 1))
     
     def forward(self):
         return self.t
-    
-        osc = torch.sigmoid(self.osc.view(self.n_atoms, self.n_frequencies, 1)) ** 2
-        freq = (osc * np.pi).repeat(1, 1, self.n_samples)
-        osc = torch.sin(torch.cumsum(freq, dim=-1))
-        osc = osc * (self.amp.view(self.n_atoms, self.n_frequencies, 1) ** 2)
+        # osc = torch.sigmoid(self.osc.view(self.n_atoms, self.n_frequencies, 1)) ** 2
+        # freq = (osc * np.pi).repeat(1, 1, self.n_samples)
+        # osc = torch.sin(torch.cumsum(freq, dim=-1))
+        # osc = osc * (self.amp.view(self.n_atoms, self.n_frequencies, 1) ** 2)
 
-        dec = 0.9 + (torch.sigmoid(self.decay.view(self.n_atoms, self.n_frequencies, 1).repeat(1, 1, self.n_frames)) * 0.099999)
+        # dec = 0.7 + (torch.sigmoid(self.decay.view(self.n_atoms, self.n_frequencies, 1).repeat(1, 1, self.n_frames)) * 0.299999)
         # dec = torch.exp(torch.cumsum(torch.log(dec), dim=-1))
-        dec = torch.cumprod(dec, dim=-1)
-        dec = F.interpolate(dec, size=self.n_samples, mode='linear')
+        # # dec = torch.cumprod(dec, dim=-1)
+        # dec = F.interpolate(dec, size=self.n_samples, mode='linear')
 
-        osc = osc * dec
-        osc = torch.sum(osc, dim=1, keepdim=True)
-        return osc.view(1, self.n_atoms, self.n_samples)
+        # osc = osc * dec
+        # osc = torch.sum(osc, dim=1, keepdim=True)
+        # return osc.view(1, self.n_atoms, self.n_samples)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.net = DilatedStack(channels, [1, 3, 9, 27, 1])
+        self.reduce = nn.Conv1d(channels + 33, channels, 1, 1, 0)
+        self.j = nn.Conv1d(channels, 1, 1, 1, 0)
+        self.apply(lambda p: exp.init_weights(p))
+    
+    def forward(self, x):
+        batch, _, n_samples = x.shape
+
+        spec = exp.fb.forward(x, normalize=False)
+        pos = pos_encoded(
+            batch, spec.shape[-1], 16, device=x.device).permute(0, 2, 1)
+        spec = torch.cat([pos, spec], dim=1)
+        spec = self.reduce(spec)
+
+        x, features = self.net.forward(spec, return_features=True)
+
+        j = self.j.forward(x)
+
+        return j, features
+
 
 
 class Model(nn.Module):
@@ -113,8 +138,6 @@ class Model(nn.Module):
         self.n_atoms = n_atoms
         self.atom_size = atom_size
 
-        # self.encoder = DilatedStack(channels, [1, 3, 9, 1], dropout=0.1)
-
         self.encoder = nn.Sequential(
             DilatedBlock(channels, 1),
             DilatedBlock(channels, 3),
@@ -122,30 +145,23 @@ class Model(nn.Module):
             DilatedBlock(channels, 27),
             DilatedBlock(channels, 1),
         )
-        
+
         self.reduce = nn.Conv1d(channels + 33, channels, 1, 1, 0)
 
         self.attn = nn.Conv1d(channels, 1, 1, 1, 0)
 
-        self.to_impulse_choice = LinearOutputStack(channels, 3, out_channels=self.n_atoms)
-        self.to_transfer_choice = LinearOutputStack(channels, 3, out_channels=self.n_atoms)
+        self.to_impulse_choice = LinearOutputStack(
+            channels, 3, out_channels=self.n_atoms)
+        self.to_transfer_choice = LinearOutputStack(
+            channels, 3, out_channels=self.n_atoms)
         self.to_mixture = LinearOutputStack(channels, 3, out_channels=1)
 
         self.impulse_dict = ImpulseDictionary(n_atoms, self.atom_size)
-        self.transfer_dict = TransferFunctionDictionary(n_atoms, exp.n_samples, 16, exp.n_samples // exp.step_size)
+        self.transfer_dict = TransferFunctionDictionary(
+            n_atoms, exp.n_samples, 16, exp.n_samples // exp.step_size)
 
         self.seq_generator = nn.Conv1d(channels, self.k_sparse, 1, 1, 0)
 
-        # self.seq_generator = ConvUpsample(
-        #     channels,
-        #     channels,
-        #     start_size=128,
-        #     end_size=exp.n_samples,
-        #     from_latent=False,
-        #     mode='learned',
-        #     out_channels=self.k_sparse,
-        #     batch_norm=False)
-        
 
         self.to_impulse = nn.Conv1d(channels, self.n_atoms, 1, 1, 0)
         self.to_transfer = nn.Conv1d(channels, self.n_atoms, 1, 1, 0)
@@ -159,7 +175,8 @@ class Model(nn.Module):
         batch, _, n_samples = x.shape
 
         spec = exp.fb.forward(x, normalize=False)
-        pos = pos_encoded(batch, spec.shape[-1], 16, device=x.device).permute(0, 2, 1)
+        pos = pos_encoded(
+            batch, spec.shape[-1], 16, device=x.device).permute(0, 2, 1)
         spec = torch.cat([pos, spec], dim=1)
         spec = self.reduce(spec)
 
@@ -167,15 +184,12 @@ class Model(nn.Module):
 
         context = torch.mean(x, dim=-1)
 
-        # d = torch.cat([self.d, torch.zeros(1, self.n_atoms, exp.n_samples - self.atom_size, device=x.device)], dim=-1)
-
         # upsample to the correct sampling rate
         seq = self.seq_generator.forward(x)
-        # avg = F.avg_pool1d(seq, 512, stride=1, padding=256)[..., :exp.n_samples]
-        # seq = seq - avg
 
         attn = torch.sigmoid(self.attn.forward(x))
-        events, indices = sparsify_vectors(x, attn, self.k_sparse, normalize=False, dense=False)
+        events, indices = sparsify_vectors(
+            x, attn, self.k_sparse, normalize=False, dense=False)
 
         impulse_choice = self.to_impulse_choice.forward(events)
         impulse_choice = choice_softmax(impulse_choice)
@@ -185,24 +199,21 @@ class Model(nn.Module):
 
         impulses = impulse_choice @ d
         impulses = torch.cat([
-            impulses, 
+            impulses,
             torch.zeros(batch, self.k_sparse, exp.n_samples - self.atom_size, device=impulses.device)], dim=-1)
 
         transfer_choice = self.to_transfer_choice.forward(events)
         transfer_choice = choice_softmax(transfer_choice)
         transfers = transfer_choice @ t
 
-        mixture = torch.sigmoid(self.to_mixture.forward(events))
-
-        # d = ((1 - mixture) * fft_convolve(impulses, transfers)) + (mixture * impulses)
         d = fft_convolve(impulses, transfers) + impulses
 
         seq = F.dropout(seq, 0.05)
         seq, indices, values = sparsify(
             seq, self.k_sparse, return_indices=True)
-    
+
         output = fft_convolve(seq, d)[..., :exp.n_samples]
-        output = torch.sum(output, dim=1, keepdim=True)        
+        output = torch.sum(output, dim=1, keepdim=True)
 
         verb = self.verb.forward(context, output)
         return verb
@@ -219,10 +230,44 @@ model = Model(
 optim = optimizer(model, lr=1e-3)
 
 
+disc = Discriminator(exp.model_dim).to(device)
+disc_optim = optimizer(disc, lr=1e-3)
+
+
+def train_gen(batch):
+    optim.zero_grad()
+    recon = model.forward(batch)
+    fake_feat, j = disc.forward(recon)
+    real_feat, j = disc.forward(batch)
+    loss = F.l1_loss(fake_feat, real_feat)
+    loss.backward()
+    optim.step()
+    return loss, recon
+
+def train_disc(batch):
+    disc_optim.zero_grad()
+    with torch.no_grad():
+        recon = model.forward(batch)
+    fj, _ = disc.forward(recon)
+    rj, _ = disc.forward(batch)
+    # loss = torch.abs(1 - rj).mean() + torch.abs(0 - fj).mean()
+    loss = least_squares_disc_loss(rj, fj)
+    loss.backward()
+    disc_optim.step()
+    return loss
+
+
+feat = PsychoacousticFeature([128] * 6).to(device)
+
 def train(batch):
     optim.zero_grad()
     recon = model.forward(batch)
-    loss = exp.perceptual_loss(recon, batch)
+
+    # loss = exp.perceptual_loss(recon, batch)
+    a, _ = feat.forward(recon)
+    b, _ = feat.forward(batch)
+
+    loss = F.mse_loss(a, b)
     loss.backward()
     optim.step()
     return loss, recon
@@ -232,3 +277,16 @@ def train(batch):
 class DynamicDictionary(BaseExperimentRunner):
     def __init__(self, stream):
         super().__init__(stream, train, exp)
+        self.real = None
+        self.fake = None
+    
+    # def run(self):
+    #     for i, item in enumerate(self.iter_items()):
+    #         self.real = item
+    #         if i % 2 == 0:
+    #             g_loss, recon = train_gen(item)
+    #             print('G', g_loss.item())
+    #             self.fake = recon
+    #         else:
+    #             d_loss = train_disc(item)
+    #             print('D', d_loss.item())
--- train/experiment_runner.py
+++ train/experiment_runner.py
@@ -3,6 +3,7 @@ from util import playable
 import numpy as np
 import zounds
 
+
 class BaseExperimentRunner(object):
     def __init__(self, stream, train, exp: Experiment):
         super().__init__()
@@ -14,22 +15,23 @@ class BaseExperimentRunner(object):
 
     def orig(self):
         return playable(self.real, self.exp.samplerate)
-    
+
     def real_spec(self):
         return np.abs(zounds.spectral.stft(self.orig()))
 
     def listen(self):
         return playable(self.fake, self.exp.samplerate)
-    
+
     def fake_spec(self):
         return np.abs(zounds.spectral.stft(self.listen()))
 
-    def hard_pos(self):
-        return self.pos.data.cpu().numpy().reshape((-1, self.exp.n_frames))
-
-    def run(self):
+    def iter_items(self):
         for i, item in enumerate(self.stream):
             item = item.view(-1, 1, self.exp.n_samples)
+            yield item
+
+    def run(self):
+        for i, item in enumerate(self.iter_items()):
             self.real = item
 
             l, r = self.train(item)
