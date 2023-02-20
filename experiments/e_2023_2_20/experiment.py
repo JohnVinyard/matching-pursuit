719750bdd4f4f42cf1181bf7089d06d5c0819ae8
John Vinyard <johnwho@gmail.com>
2023-02-20 17:19:03 -0600

    Really promising experiment using decay rather than convolution

--- experiments/e_2023_2_20/experiment.py
+++ experiments/e_2023_2_20/experiment.py
@@ -3,10 +3,14 @@ from config.experiment import Experiment
 from fft_shift import fft_shift
 from modules.dilated import DilatedStack
 from modules.fft import fft_convolve
+from modules.linear import LinearOutputStack
 from modules.normalization import ExampleNorm, unit_norm
 from modules.overfitraw import OverfitRawAudio
 from modules.pos_encode import pos_encoded
-from modules.sparse import sparsify
+from modules.psychoacoustic import PsychoacousticFeature
+from modules.reverb import ReverbGenerator
+from modules.softmax import hard_softmax
+from modules.sparse import sparsify, sparsify_vectors
 from modules.stft import stft
 from perceptual.feature import NormalizedSpectrogram
 from train.experiment_runner import BaseExperimentRunner
@@ -57,87 +61,177 @@ class ApproximateConvolution(ScriptModule):
         return x
 
 
-class PatchLoss(nn.Module):
-    def __init__(self):
-        super().__init__()
+# class PatchLoss(nn.Module):
+#     def __init__(self):
+#         super().__init__()
     
-    def forward(self, recon: Tensor, target: Tensor):
-        batch = recon.shape[0]
-        n_samples = recon.shape[-1]
-        window_size = 512
-        step_size = window_size // 2
-        windowed = \
-            recon.unfold(-1, window_size, step_size).view(batch, 1, -1, window_size) \
-            * torch.hamming_window(window_size, device=recon.device).view(1, 1, 1, window_size)
-        windowed = torch.cat([
-            windowed, 
-            torch.zeros(batch, 1, windowed.shape[2], n_samples - window_size, device=recon.device)
-        ], dim=-1)
-        fm = fft_convolve(windowed, target.view(batch, 1, 1, n_samples))[..., :n_samples]
-        positions = torch.argmax(fm, dim=-1, keepdim=True)
-        positions = positions / n_samples
-        positioned = fft_shift(windowed, positions)[..., :n_samples]
-        positioned = torch.sum(positioned, dim=2).view(batch, 1, n_samples)
+#     def forward(self, recon: Tensor, target: Tensor):
+#         batch = recon.shape[0]
+#         n_samples = recon.shape[-1]
+#         window_size = 512
+#         step_size = window_size // 2
+#         windowed = \
+#             recon.unfold(-1, window_size, step_size).view(batch, 1, -1, window_size) \
+#             * torch.hamming_window(window_size, device=recon.device).view(1, 1, 1, window_size)
+#         windowed = torch.cat([
+#             windowed, 
+#             torch.zeros(batch, 1, windowed.shape[2], n_samples - window_size, device=recon.device)
+#         ], dim=-1)
+#         fm = fft_convolve(windowed, target.view(batch, 1, 1, n_samples))[..., :n_samples]
+#         positions = torch.argmax(fm, dim=-1, keepdim=True)
+#         positions = positions / n_samples
+#         positioned = fft_shift(windowed, positions)[..., :n_samples]
+#         positioned = torch.sum(positioned, dim=2).view(batch, 1, n_samples)
 
-        return F.mse_loss(positioned, target)
+#         return F.mse_loss(positioned, target)
 
-class Model(nn.Module):
-    def __init__(self):
+
+class DecayModel(nn.Module):
+    def __init__(self, channels):
         super().__init__()
-        self.approx = ApproximateConvolution()
 
-        self.n_atoms = 512
-        self.atom_size = 512
+        self.n_envelopes = 512
+        self.n_transfer = 512
         self.k_sparse = 16
+        self.n_frames = 128
 
-        self.approx_conv_percent = torch.zeros(1).fill_(0.1)
+        self.reduce = nn.Conv1d(channels + 33, channels, 1, 1, 0)
+        self.attn = nn.Conv1d(channels, 1, 1, 1, 0)
+        self.net = DilatedStack(channels, [1, 3, 9, 27, 81, 1])
 
-        self.weights = nn.Parameter(torch.zeros(
-            1, self.n_atoms, self.atom_size).uniform_(-1, 1))
+        self.envelopes = nn.Parameter(
+            torch.zeros(self.n_envelopes, self.n_frames).uniform_(0, 1) \
+            * torch.linspace(1, 0, self.n_frames)[None, :] ** 15)
     
-    def give_atoms_unit_norm(self):
-        self.weights.data[:] = unit_norm(self.weights)
+        self.transfer = nn.Parameter(torch.zeros(self.n_transfer, exp.n_samples).uniform_(-1, 1))
+
+        self.to_envelope = LinearOutputStack(channels, 3, out_channels=self.n_envelopes)
+        self.to_transfer = LinearOutputStack(channels, 3, out_channels=self.n_transfer)
+        self.to_decay = LinearOutputStack(channels, 3, out_channels=1)
+        self.to_mix = LinearOutputStack(channels, 3, out_channels=2)
+
+        self.to_context = LinearOutputStack(channels, 3, out_channels=channels)
+
+        self.atom_map = nn.Conv1d(channels, self.k_sparse, 1, 1, 0)
+
+        self.verb = ReverbGenerator(channels, 3, exp.samplerate, exp.n_samples)
+
+        self.apply(lambda p: exp.init_weights(p))
     
     def forward(self, x):
-        n_samples = x.shape[-1]
+        batch, _, n_samples = x.shape
+        x = exp.fb.forward(x, normalize=False)
+        pos = pos_encoded(batch, n_samples, 16, device=x.device).permute(0, 2, 1)
+        x = torch.cat([pos, x], dim=1)
+        x = self.reduce(x)
+        x = self.net(x)
 
-        w = torch.cat([
-            self.weights, 
-            torch.zeros(1, self.n_atoms, n_samples - self.atom_size, device=x.device)
-        ], dim=-1)
+        context = torch.mean(x, dim=-1)
+        context = self.to_context(context)
 
-        fm = self.approx.forward(x, w, self.approx_conv_percent)
-        fm = F.dropout(fm, 0.01)
-        fm = sparsify(fm, self.k_sparse, return_indices=False)
+        am = self.atom_map.forward(x)
+        am = sparsify(am, self.k_sparse, return_indices=False)
 
+        attn = self.attn(x)
 
-        fm = F.pad(fm, (0, 1))
-        x = F.conv_transpose1d(
-            fm, 
-            self.weights.view(self.n_atoms, 1, self.atom_size), 
-            stride=1, 
-            padding=self.atom_size // 2)
+        events, indices = sparsify_vectors(
+            x, attn, self.k_sparse, normalize=False, dense=False)
+        
+        e = self.to_envelope.forward(events)
+        e = hard_softmax(e, invert=True)
+        e = e @ (self.envelopes ** 2)
+
+        t = self.to_transfer.forward(events)
+        t = hard_softmax(t, invert=True)
+        t = t @ self.transfer
+
+        d = self.to_decay.forward(events)
+        d = torch.sigmoid(d)
+
+        m = self.to_mix.forward(events)
+        m = torch.softmax(m, dim=-1)
+
+        env = torch.zeros_like(e)
+        for i in range(self.n_frames):
+            if i == 0:
+                env[:, :, i] = e[:, :, i]
+            else:
+                env[:, :, i] = e[:, :, i] + (e[:, :, i - 1] * d[:, :, 0])
+        
+        envelope = F.interpolate(e, size=exp.n_samples, mode='linear')
+        noise = torch.zeros_like(envelope).uniform_(-1, 1)
+        noise = envelope * noise
+
+        with_decay = F.interpolate(env, size=exp.n_samples, mode='linear')
+        t = with_decay * t
+
+        mixture = torch.stack([noise, t], dim=-1) * m[:, :, None, :]
+        mixture = torch.sum(mixture, dim=-1)
+        
+        x = fft_convolve(mixture, am)[..., :exp.n_samples]
+        x = torch.sum(x, dim=1, keepdim=True)
+
+        x = self.verb.forward(context, x)
         return x
 
 
-# model = Model().to(device)
+# class Model(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.approx = ApproximateConvolution()
 
-model = OverfitRawAudio((1, 1, exp.n_samples), std=0.1).to(device)
+#         self.n_atoms = 1024
+#         self.atom_size = 2048
+#         self.k_sparse = 16
+
+#         self.approx_conv_percent = torch.zeros(1).fill_(0.1)
+
+#         self.weights = nn.Parameter(torch.zeros(
+#             1, self.n_atoms, self.atom_size).uniform_(-1, 1))
+    
+#     def give_atoms_unit_norm(self):
+#         self.weights.data[:] = unit_norm(self.weights)
+    
+#     def forward(self, x):
+#         n_samples = x.shape[-1]
+
+#         w = torch.cat([
+#             self.weights, 
+#             torch.zeros(1, self.n_atoms, n_samples - self.atom_size, device=x.device)
+#         ], dim=-1)
+
+#         fm = self.approx.forward(x, w, self.approx_conv_percent)
+#         fm = F.dropout(fm, 0.01)
+#         fm = sparsify(fm, self.k_sparse, return_indices=False)
+
+
+#         fm = F.pad(fm, (0, 1))
+#         x = F.conv_transpose1d(
+#             fm, 
+#             self.weights.view(self.n_atoms, 1, self.atom_size), 
+#             stride=1, 
+#             padding=self.atom_size // 2)
+#         return x
+
+
+model = DecayModel(exp.model_dim).to(device)
 optim = optimizer(model, lr=1e-3)
 
-loss_model = PatchLoss()
+feat = PsychoacousticFeature().to(device)
+
+def experiment_loss(a, b):
+    a, _ = feat.forward(a)
+    b, _ = feat.forward(b)
+    return F.mse_loss(a, b)
 
 def train(batch):
-    # model.give_atoms_unit_norm()
     optim.zero_grad()
-    batch_size = batch.shape[0]
     recon = model.forward(batch)
     # loss = F.mse_loss(recon, batch)
-    loss = loss_model.forward(recon, batch)
+    loss = experiment_loss(recon, batch)
     loss.backward()
     optim.step()
     return loss, recon
-
 
 @readme
 class ApproximateConvolution(BaseExperimentRunner):
