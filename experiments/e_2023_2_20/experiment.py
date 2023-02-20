772d670d2fa7857df6435555d9c11da72a379303
John Vinyard <johnwho@gmail.com>
2023-02-20 15:23:45 -0600

    Try out patch-based loss.  It doesn't really work or make much sense after all

--- experiments/e_2023_2_20/experiment.py
+++ experiments/e_2023_2_20/experiment.py
@@ -1,8 +1,10 @@
 from typing import Callable, Type
 from config.experiment import Experiment
+from fft_shift import fft_shift
 from modules.dilated import DilatedStack
 from modules.fft import fft_convolve
-from modules.normalization import ExampleNorm
+from modules.normalization import ExampleNorm, unit_norm
+from modules.overfitraw import OverfitRawAudio
 from modules.pos_encode import pos_encoded
 from modules.sparse import sparsify
 from modules.stft import stft
@@ -18,6 +20,7 @@ import torch
 import numpy as np
 from torch.jit._script import ScriptModule, script_method
 import time
+from torch.nn.init import orthogonal_
 
 
 exp = Experiment(
@@ -54,62 +57,86 @@ class ApproximateConvolution(ScriptModule):
         return x
 
 
-
-approx = ApproximateConvolution()
-
-# def approx_conv(a, b, percent_sparse):
-
-#     n_samples = a.shape[-1]
-
-
-#     a = F.pad(a, (0, a.shape[-1]))
-#     b = F.pad(b, (0, b.shape[-1]))
-
-#     n_coeffs = ((a.shape[-1] // 2) + 1)
-#     n_elements = int(n_coeffs * percent_sparse)
-#     # indices = torch.randperm(n_coeffs, device=a.device)[:n_elements]
-#     indices = torch.arange(0, n_elements)
-
-#     a_spec = torch.fft.rfft(a, dim=-1, norm='ortho')
-#     b_spec = torch.fft.rfft(b, dim=-1, norm='ortho')
-
-#     x = a_spec * b_spec
-
-#     x = torch.zeros_like(a_spec)
-#     for index in indices:
-#         x[:, :, index] = a_spec[:, :, index] * b_spec[:, :, index]
+class PatchLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
     
-#     x = torch.fft.irfft(x, dim=-1, norm='ortho')[..., :n_samples]
+    def forward(self, recon: Tensor, target: Tensor):
+        batch = recon.shape[0]
+        n_samples = recon.shape[-1]
+        window_size = 512
+        step_size = window_size // 2
+        windowed = \
+            recon.unfold(-1, window_size, step_size).view(batch, 1, -1, window_size) \
+            * torch.hamming_window(window_size, device=recon.device).view(1, 1, 1, window_size)
+        windowed = torch.cat([
+            windowed, 
+            torch.zeros(batch, 1, windowed.shape[2], n_samples - window_size, device=recon.device)
+        ], dim=-1)
+        fm = fft_convolve(windowed, target.view(batch, 1, 1, n_samples))[..., :n_samples]
+        positions = torch.argmax(fm, dim=-1, keepdim=True)
+        positions = positions / n_samples
+        positioned = fft_shift(windowed, positions)[..., :n_samples]
+        positioned = torch.sum(positioned, dim=2).view(batch, 1, n_samples)
 
-#     return x
+        return F.mse_loss(positioned, target)
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.approx = ApproximateConvolution()
+
+        self.n_atoms = 512
+        self.atom_size = 512
+        self.k_sparse = 16
+
+        self.approx_conv_percent = torch.zeros(1).fill_(0.1)
+
+        self.weights = nn.Parameter(torch.zeros(
+            1, self.n_atoms, self.atom_size).uniform_(-1, 1))
+    
+    def give_atoms_unit_norm(self):
+        self.weights.data[:] = unit_norm(self.weights)
+    
+    def forward(self, x):
+        n_samples = x.shape[-1]
+
+        w = torch.cat([
+            self.weights, 
+            torch.zeros(1, self.n_atoms, n_samples - self.atom_size, device=x.device)
+        ], dim=-1)
+
+        fm = self.approx.forward(x, w, self.approx_conv_percent)
+        fm = F.dropout(fm, 0.01)
+        fm = sparsify(fm, self.k_sparse, return_indices=False)
+
+
+        fm = F.pad(fm, (0, 1))
+        x = F.conv_transpose1d(
+            fm, 
+            self.weights.view(self.n_atoms, 1, self.atom_size), 
+            stride=1, 
+            padding=self.atom_size // 2)
+        return x
+
+
+# model = Model().to(device)
+
+model = OverfitRawAudio((1, 1, exp.n_samples), std=0.1).to(device)
+optim = optimizer(model, lr=1e-3)
+
+loss_model = PatchLoss()
 
 def train(batch):
+    # model.give_atoms_unit_norm()
+    optim.zero_grad()
     batch_size = batch.shape[0]
-
-    weights = torch.zeros(1, 256, batch.shape[-1], device=batch.device)
-
-    # shuffled = batch[batch_size // 2:]
-    # batch = batch[:batch_size // 2]
-    # indices = torch.randperm(batch.shape[0], device=batch.device)
-    # shuffled = batch[indices]
-
-    start = time.monotonic()
-    real_fm = fft_convolve(batch, weights)
-    best = torch.argmax(real_fm, dim=-1)
-    print(best.data.cpu().numpy().squeeze()) 
-    print(f'Full convolution took {time.monotonic() - start}')
-
-    start = time.monotonic()
-    approx_fm = approx.forward(
-        batch, weights, torch.zeros(1, device=batch.device).fill_(0.1))
-    approx_best = torch.argmax(approx_fm, dim=-1)
-    print(approx_best.data.cpu().numpy().squeeze())
-    print(f'Approx. convolution took {time.monotonic() - start}')
-
-    
-    input('Waiting...')
-
-    return torch.zeros(1).fill_(0), None
+    recon = model.forward(batch)
+    # loss = F.mse_loss(recon, batch)
+    loss = loss_model.forward(recon, batch)
+    loss.backward()
+    optim.step()
+    return loss, recon
 
 
 @readme
