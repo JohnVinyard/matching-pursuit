c209d9f89e13b00223fa1a2d32b5112333588ad7
John Vinyard <johnwho@gmail.com>
2023-03-31 08:47:40 -0500

    Filter bank resonance experiment working decently weel

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,3 +1,4 @@
 # from .e_2023_3_22 import MatchingPursuitGAN as Current
 # from .e_2023_3_8 import BasicMatchingPursuit as Current
-from .e_2023_3_30 import ScalarPositioning as Current
+# from .e_2023_3_30 import ScalarPositioning as Current
+from .e_2023_3_30_B import BandFilteredImpulseResponse as Current
