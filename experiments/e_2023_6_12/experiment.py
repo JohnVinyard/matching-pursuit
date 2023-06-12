39dfeac28d00f2e64386577d8690e869a4e1223e
John Vinyard <johnwho@gmail.com>
2023-06-12 16:46:46 -0500

    Refactor to make it slightly easier to try different upsampling methods

--- experiments/e_2023_6_12/experiment.py
+++ experiments/e_2023_6_12/experiment.py
@@ -6,6 +6,7 @@ import zounds
 from config.experiment import Experiment
 from modules.atoms import unit_norm
 from modules.phase import AudioCodec, MelScale
+from modules.sparse import sparsify
 from train.experiment_runner import BaseExperimentRunner
 from train.optim import optimizer
 from upsample import ConvUpsample, FFTUpsampleBlock
@@ -21,7 +22,24 @@ exp = Experiment(
     kernel_size=512)
 
 
+class UpsampleBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        batch, channels, time = x.shape
 
+        return F.interpolate(x, scale_factor=4, mode='nearest')
+        
+        new_time = time * 4
+        new_coeffs = new_time // 2 + 1
+
+        spec = torch.fft.rfft(x, dim=-1, norm='ortho')
+        new_spec = torch.zeros(batch, channels, new_coeffs, dtype=spec.dtype, device=spec.device)
+        new_spec[:, :, :spec.shape[-1]] = spec
+
+        result = torch.fft.irfft(new_spec, dim=-1, norm='ortho')
+        return result
 
 class Model(nn.Module):
     def __init__(self):
@@ -29,40 +47,65 @@ class Model(nn.Module):
 
         self.embed = nn.Linear(257, 8)
 
+        self.prepare = nn.Sequential(
+            nn.Conv1d(1024, 256, 3, padding=1, dilation=1),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv1d(256, 256, 3, padding=3, dilation=3),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv1d(256, 256, 3, padding=9, dilation=9),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv1d(256, 256, 3, padding=27, dilation=27),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv1d(256, 1024, 1, 1, 0)
+        )
+
         self.up = nn.Sequential(
 
             nn.Conv1d(1024, 512, 7, 1, 3),
             nn.BatchNorm1d(512),
             nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=4, mode='nearest'), # 512
+            # nn.Upsample(scale_factor=4, mode='nearest'), # 512
+            UpsampleBlock(),
 
             nn.Conv1d(512, 256, 7, 1, 3),
             nn.BatchNorm1d(256),
             nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=4, mode='nearest'), # 2048
+            # nn.Upsample(scale_factor=4, mode='nearest'), # 2048
+            UpsampleBlock(),
 
             nn.Conv1d(256, 128, 7, 1, 3),
             nn.BatchNorm1d(128),
             nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=4, mode='nearest'), # 8192
+            # nn.Upsample(scale_factor=4, mode='nearest'), # 8192
+            UpsampleBlock(),
 
             nn.Conv1d(128, 64, 7, 1, 3),
             nn.BatchNorm1d(64),
             nn.LeakyReLU(0.2),
-            nn.Upsample(scale_factor=4, mode='nearest'), # 32768
+            # nn.Upsample(scale_factor=4, mode='nearest'), # 32768
+            UpsampleBlock(),
 
             nn.Conv1d(64, 1, 7, 1, 3),
         )        
         self.apply(lambda x: exp.init_weights(x))
     
     def forward(self, x):
-        if self.pif_input:
-            # torch.Size([16, 128, 128, 257])
-            batch, channels, time, period = x.shape
-            x = self.embed(x).permute(0, 3, 1, 2).reshape(batch, 8 * channels, time)
-        else:
-            batch, time, channels = x.shape
-            x = self.embed(x)
+        # torch.Size([16, 128, 128, 257])
+        batch, channels, time, period = x.shape
+        x = self.embed(x).permute(0, 3, 1, 2).reshape(batch, 8 * channels, time)
+
+        # sparsify
+        # x = self.prepare(x)
+        # x = sparsify(x, n_to_keep=128, return_indices=False)
+
         
         x = self.up(x)
         return x
