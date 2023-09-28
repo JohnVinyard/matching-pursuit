c291b5e858793b27b478ce8ae5385b62e5b2444a
John Vinyard <johnwho@gmail.com>
2023-09-27 20:40:58 -0500

    This is the stopping point if multi-band generator doesn't help

--- experiments/e_2023_9_23/experiment.py
+++ experiments/e_2023_9_23/experiment.py
@@ -5,11 +5,14 @@ from torch import nn
 from torch.nn import functional as F
 import zounds
 from config.experiment import Experiment
+from modules.ddsp import AudioModel
 from modules.normalization import max_norm
 from modules.pos_encode import pos_encoded
+from modules.reverb import ReverbGenerator
 from modules.sparse import sparsify
 from time_distance import optimizer
 from train.experiment_runner import BaseExperimentRunner, MonitoredValueDescriptor
+from upsample import ConvUpsample
 from util import device
 from util.readmedocs import readme
 
@@ -23,24 +26,92 @@ exp = Experiment(
     windowed_pif=True)
 
 
-class Contextual(nn.Module):
-    def __init__(self, channels):
+class Discriminator(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.channels = channels
-        layers = 6
-        heads = 4
-        self.down = nn.Conv1d(channels + 33, channels, 1, 1, 0)
-        encoder = nn.TransformerEncoderLayer(channels, heads, channels, batch_first=True)
-        self.encoder = nn.TransformerEncoder(encoder, num_layers=layers, norm=nn.LayerNorm((128, channels)))
 
-    def forward(self, x):
-        pos = pos_encoded(x.shape[0], x.shape[-1], 16, device=x.device).permute(0, 2, 1)
-        x = torch.cat([x, pos], dim=1)
+        self.embed_cond = nn.Conv1d(4096, 1024, 1, 1, 0)
+        self.embed_spec = nn.Conv1d(exp.n_bands, 1024, 1, 1, 0)
+        self.down = nn.Conv1d(1024 * 2, 1024, 1, 1, 0)
+
+        self.encoder = nn.Sequential(
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 3, 1, padding=1, dilation=1),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 3, 1, padding=3, dilation=3),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 3, 1, padding=9, dilation=9),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 3, 1, padding=1, dilation=1),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            # 32
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 7, 4, 3),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            # 8
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 7, 4, 3),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            # 2
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 7, 4, 3),
+                nn.LeakyReLU(0.2),
+                nn.BatchNorm1d(1024)
+            ),
+
+            # 1
+            nn.Sequential(
+                nn.Dropout(0.05),
+                nn.Conv1d(1024, 1024, 2, 2, 0),
+                nn.LeakyReLU(0.2),
+                # nn.BatchNorm1d(1024)
+            ),
+
+            nn.Conv1d(1024, 1, 1, 1, 0)
+            
+        )
+
+        self.apply(lambda x: exp.init_weights(x))
+    
+    def forward(self, cond: torch.Tensor, audio: torch.Tensor):
+        spec = exp.pooled_filter_bank(audio)
+        spec = self.embed_spec(spec)
+        cond = self.embed_cond(cond)
+        x = torch.cat([spec, cond], dim=1)
+        # x = spec + cond
         x = self.down(x)
-        x = x.permute(0, 2, 1)
         x = self.encoder(x)
-        x = x.permute(0, 2, 1)
+        
         return x
+
     
 class Model(nn.Module):
     def __init__(self):
@@ -74,6 +145,7 @@ class Model(nn.Module):
                 nn.LeakyReLU(0.2),
                 nn.BatchNorm1d(1024)
             ),
+
             nn.Conv1d(1024, 4096, 1, 1, 0)
         )
 
@@ -147,11 +219,13 @@ class Model(nn.Module):
                 nn.Conv1d(2, 1, 7, 1, 3),
             ),
         )
+        
+
+
 
         self.apply(lambda x: exp.init_weights(x))
-
-    def forward(self, x):
-
+    
+    def encode(self, x):
         batch_size = x.shape[0]
 
         if len(x.shape) != 4:
@@ -161,13 +235,25 @@ class Model(nn.Module):
         x = x.permute(0, 3, 1, 2)
         x = x.reshape(batch_size, 8 * exp.n_bands, -1)
         encoded = self.encoder.forward(x)
+        return encoded
+
+    def forward(self, x):
+
+        # batch_size = x.shape[0]
+
+        # if len(x.shape) != 4:
+        #     x = exp.perceptual_feature(x)
+
+        # x = self.embed(x)
+        # x = x.permute(0, 3, 1, 2)
+        # x = x.reshape(batch_size, 8 * exp.n_bands, -1)
+        # encoded = self.encoder.forward(x)
+        encoded = self.encode(x)
 
         encoded = sparsify(encoded, n_to_keep=512)
 
         decoded = self.decoder.forward(encoded)
-
         final = self.up.forward(decoded)
-
         return final, encoded
 
 
@@ -175,23 +261,56 @@ model = Model().to(device)
 optim = optimizer(model, lr=1e-3)
 
 
+disc = Discriminator().to(device)
+disc_optim = optimizer(disc, lr=1e-3)
+
+
 def train(batch, i):
     optim.zero_grad()
+    disc_optim.zero_grad()
 
     with torch.no_grad():
         feat = exp.perceptual_feature(batch)
 
-    recon, encoded = model.forward(feat)
+    if i % 2 == 0:
 
-    r = exp.perceptual_feature(recon)
-    loss = F.mse_loss(r, feat)
+        # get conditioning signal
+        with torch.no_grad():
+            recon, encoded = model.forward(feat)
+        
+        rj = disc.forward(encoded, batch)
+        fj = disc.forward(encoded, recon)
+        loss = (torch.abs(1 - rj) + torch.abs(0 - fj)).mean()
+        loss.backward()
+        disc_optim.step()
+        print(i, 'DISC', loss.item())
+        return None, None, None
+    else:
+        recon, encoded = model.forward(feat)
+        r = exp.perceptual_feature(recon)
 
-    loss.backward()
-    optim.step()
+        cond = encoded.clone().detach()
+        fj = disc.forward(cond, recon)
 
-    recon = max_norm(recon)
+        # MSE loss and adversarial loss
+        loss = torch.abs(1 - fj).mean() + F.mse_loss(r, feat)
+        loss.backward()
+        optim.step()
+        print(i, 'GEN', loss.item())
+        return loss, recon, encoded
 
-    return loss, recon, encoded
+
+    # recon, encoded = model.forward(feat)
+
+    # r = exp.perceptual_feature(recon)
+    # loss = F.mse_loss(r, feat)
+
+    # loss.backward()
+    # optim.step()
+
+    # recon = max_norm(recon)
+
+    # return loss, recon, encoded
 
 
 def make_conjure(experiment: BaseExperimentRunner):
@@ -218,8 +337,12 @@ class SparseV5(BaseExperimentRunner):
         for i, item in enumerate(self.iter_items()):
             item = item.view(-1, 1, exp.n_samples)
             l, r, e = train(item, i)
+
+            if l is None:
+                continue
+
             self.real = item
             self.fake = r
             self.encoded = e
-            print(i, l.item())
+            # print(i, l.item())
             self.after_training_iteration(l)
--- experiments/e_2023_9_23/readme.md
+++ experiments/e_2023_9_23/readme.md
@@ -3,9 +3,11 @@ What if sparsity happens at pif feature rate?
 
 
 # Things to try:
-- transformer context module
-- reverb
-- adversarial loss
+- ~~transformer context module~~
+- ~~reverb~~
+- ~~filter bank front-end~~
+- ~~audio front-end~~
+- **adversarial loss**
 - image segmentation loss
 - context vector
 
@@ -14,3 +16,19 @@ What if sparsity happens at pif feature rate?
 11,121 0.1798892319202423
 
 # Transformer Context
+Transformer experiments aborted early on, sounds terrible
+
+# Reverb
+15,678 0.16117845475673676
+
+Comparable to vanilla, maybe subjectively slightly worse
+
+# Filter Bank Front End
+Aborted, sounds cartoon-y and sounds worse than "Raw" convolutional model
+
+# Audio Model
+Cartoon-y, but better than filter bank front end
+
+# Adversarial Loss
+Going to 22K iteratiosn to be fair.  It's unclear whether this is better.
+MSE might be just fine.
