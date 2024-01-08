dd2cb71246b707298620f31d65c3fae20ecddde4
John Vinyard <johnwho@gmail.com>
2024-01-08 13:24:35 -0600

    Context vector

--- model-demo/components/Vector.tsx
+++ model-demo/components/Vector.tsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Slider,
+  Typography,
+} from "@mui/material";
 
 interface VectorProps {
   vector: number[];
+  min: number;
+  max: number;
+  onVectorChange: (vector: number[]) => void;
+  readonly: boolean;
 }
 
-const Vector: React.FC<VectorProps> = ({ vector }) => {
-  const [date, setDate] = useState<Date>(new Date());
-
-  const update = () => {
-    setDate(new Date());
+const Vector: React.FC<VectorProps> = ({
+  vector,
+  min,
+  max,
+  onVectorChange,
+  readonly,
+}) => {
+  const sliderChange = (newValue: number, index: number) => {
+    const newVector = [...vector];
+    newVector[index] = newValue;
+    onVectorChange(newVector);
   };
 
   return (
-    <div>
-      <div>Oh hai, it's {date.toString()}</div>
-      <button onClick={update}>Update</button>
-    </div>
+    <Card>
+      <CardHeader
+        title={<Typography variant="h6">Context Vector</Typography>}
+      ></CardHeader>
+      <CardContent>
+        <Grid container direction="row" spacing={1}>
+          {vector.map((v, i) => (
+            <Grid item>
+              <Slider
+                size="medium"
+                orientation="vertical"
+                style={{ height: "100px", width: "20px" }}
+                disabled={!readonly}
+                min={min}
+                max={max}
+                value={v}
+                onChangeCommitted={(event, value) => sliderChange(v, i)}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
