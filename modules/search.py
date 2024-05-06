2a3bf60ebb8839275e3b723f5d34316120adf7eb
John Vinyard <johnwho@gmail.com>
2024-05-06 18:30:38 -0500

    First step toward visualizing embeddings by projecting to a lower dimension

--- modules/search.py
+++ modules/search.py
@@ -1,5 +1,6 @@
 from typing import List
 import torch
+from torch.nn.init import orthogonal_
 
 def k_nearest(
         query: torch.Tensor, 
@@ -8,7 +9,6 @@ def k_nearest(
     
     n_items, dim = embeddings.shape
     query = query.view(1, dim)
-    
     dist = torch.cdist(query, embeddings)
     dist = dist.view(n_items)
     indices = torch.argsort(dist)
@@ -16,11 +16,27 @@ def k_nearest(
 
 
 class BruteForceSearch(object):
-    def __init__(self, embeddings: torch.Tensor, keys: List[str], n_results: int):
+    def __init__(
+            self, 
+            embeddings: torch.Tensor, 
+            keys: List[str], 
+            n_results: int, 
+            visualization_dim: int):
+        
         super().__init__()
         self.embeddings = embeddings
         self.n_results = n_results
         self.keys = keys
+        self.visualization_dim = visualization_dim
+        proj = torch.zeros(
+            embeddings.shape[-1], visualization_dim, device=embeddings.device)
+        orthogonal_(proj)
+        self.projection = proj
+    
+    def visualization(self):
+        proj = self.embeddings @ self.projection
+        assert proj.shape == (self.embeddings.shape[0], self.visualization_dim)
+        return proj
     
     def search(self, query: torch.Tensor):
         indices = k_nearest(query, self.embeddings, n_results=self.n_results)
--- search.py
+++ search.py
@@ -4,13 +4,18 @@ from experiments.e_2024_3_21.inference import model, ResonanceInferenceModel
 import zounds
 import torch
 from modules.normalization import unit_norm
-from modules.pointcloud import GraphEdgeEmbedding
+# from modules.pointcloud import GraphEdgeEmbedding
 from modules.search import BruteForceSearch
 from util.playable import listen_to_sound, playable
 import conjure
 import os
 from util import device
 import numpy as np
+from time import time
+from matplotlib import pyplot as plt
+import matplotlib
+
+matplotlib.use('Qt5Agg', force=True)
 
 n_samples = 2**15
 samplerate = zounds.SR22050()
@@ -19,18 +24,32 @@ model: ResonanceInferenceModel = model.to(device)
 
 collection = conjure.LmdbCollection('search')
 
-graph_embedding = GraphEdgeEmbedding(16, 16, 128).to(device)
+# graph_embedding = GraphEdgeEmbedding(16, 16, 128).to(device)
 
 def compute_embedding(model: ResonanceInferenceModel, batch: torch.Tensor) -> torch.Tensor:
     with torch.no_grad():
         channels, encoding, schedules = model.iterative(batch)
-        embeddings = graph_embedding.forward(encoding)
+        
+        b, n_points, dim = encoding.shape
+        
+        embeddings = torch.sum(encoding, dim=1)
+        
+        # embeddings = graph_embedding.forward(encoding)
         return embeddings
 
 
-@conjure.pickle_conjure(
-    collection, 
-    read_hook=lambda x: print('reading build_index() from cache'))
+# @conjure.pickle_conjure(
+#     collection, 
+#     read_hook=lambda x: print('reading build_index() from cache'))
+
+@conjure.conjure(
+    content_type='application/octet-steam', 
+    storage=collection,
+    func_identifier=conjure.LiteralFunctionIdentifier('index'),
+    param_identifier=conjure.LiteralParamsIdentifier('index_params'),
+    serializer=conjure.PickleSerializer(),
+    deserializer=conjure.PickleDeserializer(),
+    read_from_cache_hook=lambda x: print('reading build_index() from cache'))
 def build_index(model: ResonanceInferenceModel, index_size_limit: int) -> dict:
     
     def make_key(full_path: str, start: int, stop: int) -> str:
@@ -48,10 +67,12 @@ def build_index(model: ResonanceInferenceModel, index_size_limit: int) -> dict:
             device=device, 
             make_key=make_key):
         
+        start_time = time()
         embedding = compute_embedding(model, chunk)
         embedding = embedding.data.cpu().numpy().reshape(1, -1)
+        end_time = time()
         
-        print('\t', key, chunk.shape, embedding.shape)
+        print('\t', key, chunk.shape, embedding.shape, f'{end_time - start_time:.2f} seconds')
         
         keys.append(key)
         embeddings.append(embedding)
@@ -83,29 +104,51 @@ def main(model: ResonanceInferenceModel):
         return slice(int(start), int(end))
     
         
-    keys = db['keys']
+    db_keys = db['keys']
     embeddings = db['embeddings']
     
-    total_items = len(keys)
+    total_items = len(db_keys)
     n_examples = 10
     
     embeddings = torch.from_numpy(embeddings).float()
     embeddings = unit_norm(embeddings)
     
     
-    search = BruteForceSearch(embeddings, keys, n_results=5)
+    search = BruteForceSearch(embeddings, db_keys, n_results=5, visualization_dim=2)
+    
+    points = search.visualization().data.cpu().numpy()
+    
+    
+    plt.scatter(points[:, 0], points[:, 1], s=0.1)
+    plt.show()
+    plt.clf()
     
     for i in range(n_examples):
         print('==============================================')
         
         query_index = np.random.randint(0, total_items)
+        
+        
         query = embeddings[query_index]
+        query_key = db_keys[query_index]
+        
         keys, e = search.search(query)
+        
+        e = e.data.cpu().numpy()
+        
+        plt.scatter(e[:, 0], e[:, 1])
+        ax = plt.gca()
+        ax.set_xlim([-2, 2])
+        ax.set_ylim([-2, 2])
+        plt.show()
+        plt.clf()
         
         for key, embedding in zip(keys, e):
             fp = filepath_from_key(key)
             slce = slice_from_key(key)
             chunk = load_audio_chunk(fp, slce, device=embeddings.device)
+            
+            print(f'Query key {query_key}, result key {key}')    
             
             p: zounds.AudioSamples = playable(chunk, samplerate, normalize=True)
             listen_to_sound(p, wait_for_user_input=True)
