fe647b151dc228a09dd5b3fca45db2b5f042dc85
John Vinyard <johnwho@gmail.com>
2024-08-14 11:50:20 -0500

    Try an approach that stacks the control plane-based model

--- scratch3.py
+++ scratch3.py
@@ -1,5 +1,6 @@
 from base64 import b64encode
 from subprocess import PIPE, Popen
+from typing import List
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -254,7 +255,6 @@ class Instrument3(nn.Module):
             self, 
             encoding_channels: int, 
             channels: int, 
-            n_layers: int, 
             n_frames: int, 
             n_samples: int,
             shape_channels: int):
@@ -262,18 +262,17 @@ class Instrument3(nn.Module):
         super().__init__()
         self.encoding_channels = encoding_channels
         self.channels = channels
-        self.n_layers = n_layers
         self.n_frames = n_frames
         self.n_samples = n_samples
         self.shape_channels = shape_channels
         
         self.hyper = HyperNetworkLayer(
-            shape_channels, 16, channels, encoding_channels)
+            shape_channels, 64, channels, encoding_channels)
         
     def _pos_encoding(self, n_samples: int):
         """Returns a filterbank with periodic functions
         """
-        freqs = torch.linspace(0.00001, 0.49, steps=self.encoding_channels)
+        freqs = torch.linspace(0.00001, 0.49, steps=self.encoding_channels) ** 2
         t = torch.linspace(0, n_samples, steps=n_samples)
         p = torch.sin(t[None, :] * freqs[:, None] * np.pi)
         p = p.view(1, 1, self.encoding_channels, self.n_samples)
@@ -299,7 +298,11 @@ class Instrument3(nn.Module):
         envelopes = envelopes.view(batch, n_events, cp, frames)
         
         energy = fft_convolve(energy, envelopes)
+        energy = torch.tanh(energy)
+        orig_energy = energy
+        
         energy = energy.permute(0, 1, 3, 2)
+        
 
         # the shape describes how the control plane translates into
         # a mixture of resonators        
@@ -319,21 +322,114 @@ class Instrument3(nn.Module):
         transformed = transformed.view(batch, n_events, self.encoding_channels, self.n_samples)
         
         
-        # TODO: This could be a "deep", non-linear transformation,
-        # rather than a linear one
-        # transformed = energy @ transforms
-        # torch.relu(transformed)
-        
-        # transformed = transformed.permute(0, 1, 3, 2).view(batch * n_events, self.encoding_channels, frames)
-        # transformed = F.interpolate(transformed, size=self.n_samples, mode='linear')
-        # transformed = transformed.view(batch, n_events, self.encoding_channels, self.n_samples)
-        
         final = pos * transformed
         final = torch.sum(final, dim=2)
-        return final
         
+        return final, orig_energy
+        
+class InstrumentStack(nn.Module):
+    def __init__(
+            self, 
+            encoding_channels: int, 
+            channels: int, 
+            n_frames: int, 
+            n_samples: int,
+            shape_channels: int,
+            n_layers: int):
+        
+        super().__init__()
+        self.encoding_channels = encoding_channels
+        self.channels = channels
+        self.n_frames = n_frames
+        self.n_samples = n_samples
+        self.shape_channels = shape_channels
+        self.n_layers = n_layers
+        
+        self.layers = nn.ModuleList([
+            Instrument3(
+                encoding_channels, 
+                channels, 
+                n_frames, 
+                n_samples, 
+                shape_channels) 
+            for _ in range(self.n_layers)
+        ])
+    
+    def forward(
+            self, 
+            energy: torch.Tensor, 
+            transforms: List[torch.Tensor],
+            decays: List[torch.Tensor],
+            mix: torch.Tensor):
+        
+        batch, n_events, layers = mix.shape
+        
+        batch, n_events, channels, frames = energy.shape
+        
+        e = energy
+        output = torch.zeros(batch, n_events, self.n_layers, self.n_samples)
+        
+        for i, layer in enumerate(self.layers):
+            print(i, e.shape)
+            audio, e = layer.forward(e, transforms[i], decays[i])
+            output[:, :, i, :] = audio
+        
+        mx = torch.softmax(mix, dim=-1)
+        
+        output = output * mx[:, :, :, None]
+        output = torch.sum(output, dim=2)
+        return output
 
-        
+
+def tryout_instrument_stack():
+    batch_size = 2
+    n_events = 4
+    control_plane = 32
+    shape_channels = 16
+    encoding_channels = 512
+    n_frames = 128
+    n_samples = 2**15
+    n_shape_frames = 4
+    layers = 4
+    
+    energy = torch.zeros(
+        batch_size, n_events, control_plane, n_frames).bernoulli_(p=0.001)
+    energy = energy * torch.zeros_like(energy).uniform_(0, 10)
+    
+    shapes = [
+        torch.zeros(
+            batch_size, n_events, shape_channels, n_shape_frames).bernoulli_(p=0.24) 
+        for _ in range(layers)
+    ]
+    
+    decays = [
+        torch.zeros(batch_size, n_events, control_plane, 1).uniform_(0.5, 0.6) 
+        for _ in range(layers)
+    ]
+    
+    mix = torch.zeros(batch_size, n_events, layers).uniform_(-1, 1)
+    
+    instr = InstrumentStack(
+        encoding_channels=encoding_channels,
+        channels=control_plane,
+        n_frames=n_frames,
+        n_samples=n_samples,
+        shape_channels=shape_channels,
+        n_layers=layers
+    )
+    
+    audio = instr.forward(
+        energy=energy,
+        transforms=shapes,
+        decays=decays,
+        mix=mix
+    )
+    
+    audio = audio[0, 0].data.cpu().numpy()
+    audio = audio / audio.max()
+    
+    listen_to_sound(audio, 22050, wait_for_user_input=True)
+    
 def tryout_instrument3():
     
     batch_size = 2
@@ -343,7 +439,7 @@ def tryout_instrument3():
     encoding_channels = 512
     n_frames = 128
     n_samples = 2**15
-    n_shape_frames = 16
+    n_shape_frames = 4
     
     energy = torch.zeros(
         batch_size, n_events, control_plane, n_frames).bernoulli_(p=0.001)
@@ -354,17 +450,14 @@ def tryout_instrument3():
     
     decays = torch.zeros(batch_size, n_events, control_plane, 1).uniform_(0.5, 0.99)
     
-    # transforms = torch.zeros(batch_size, n_events, control_plane, encoding_channels).uniform_(-1, 1)
-    
     inst = Instrument3(
         encoding_channels, 
         control_plane, 
-        n_layers=4, 
         n_frames=n_frames,
         n_samples=n_samples,
         shape_channels=shape_channels)
     
-    result = inst.forward(
+    result, energy = inst.forward(
         energy=energy, 
         transforms=shape, 
         decays=decays)
@@ -384,7 +477,6 @@ def test_shift():
     plt.plot(signal)
     plt.show()
     
-    
     shifted = fft_shift(signal, torch.zeros(1).fill_(0.5))
     plt.plot(shifted)
     plt.show()
@@ -393,11 +485,12 @@ def test_shift():
     assert index.item() == 64
 
 
-
 if __name__ == '__main__':
     
     
-    tryout_instrument3()
+    tryout_instrument_stack()
+    
+    # tryout_instrument3()
     
     # TODO: 
     # values = 1 - (np.linspace(0, 1) ** 4)
