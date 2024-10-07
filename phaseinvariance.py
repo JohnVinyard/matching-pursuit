20d2f321c5391706abaec29911076cb22a4135d7
John Vinyard <johnwho@gmail.com>
2024-10-07 13:32:11 -0500

    Getting some basic artifacts in place for phase-invariant feature article

--- phaseinvariance.py
+++ phaseinvariance.py
@@ -12,14 +12,32 @@ https://www.di.ens.fr/data/scattering/
 https://www.di.ens.fr/data/scattering/audio/
 
 """
+from modules import gammatone_filter_bank
+from modules.aim import auditory_image_model
+from util import device
+
+"""[markdown]
+
+For this experiment, we'll just be using a single piece of source audio from the 
+[MusicNet dataset](https://zenodo.org/records/5120004#.Yhxr0-jMJBA).
+
+We'll explore what different audio transformations "hear" by overfitting raw audio samples to minimize
+the loss between the transform of the original audio and the transform of the raw audio samples.
+"""
+
+# source
+
+
 from typing import Tuple
 
-from conjure import S3Collection, Logger, numpy_conjure, conjure_article
+from conjure import S3Collection, Logger, numpy_conjure, conjure_article, AudioComponent, ImageComponent
 from data import get_audio_segment
 import numpy as np
 import torch
 from argparse import ArgumentParser
 from scipy.signal import stft
+from matplotlib import cm
+
 
 collection = S3Collection(
     'phase-invariant-feature',
@@ -52,15 +70,26 @@ def spectrogram(
         audio: np.ndarray,
         window_size: int = 2048,
         step_size: int = 256,
-        mag_only: bool = False) -> np.ndarray:
+        mag_only: bool = False,
+        normalize: bool = False) -> np.ndarray:
 
     _, _, spec = stft(audio, nperseg=window_size, noverlap=window_size - step_size)
     coeffs, frames = spec.shape
 
     if mag_only:
-        return np.abs(spec)
+        mag = np.abs(spec)
+
+        if normalize:
+            mag /= mag.std(axis=1, keepdims=True)
+
+        return mag
+
+
 
     return spec
+
+
+# mag_spec
 
 
 """[markdown]
@@ -79,13 +108,15 @@ We visualize a feature that also displays frame-to-frame phase deltas
 def to_magnitude_and_phase_delta(
         audio: np.ndarray,
         window_size: int = 2048,
-        step_size: int = 256) -> Tuple[np.ndarray, np.ndarray]:
+        step_size: int = 256,
+        normalize: bool = False) -> Tuple[np.ndarray, np.ndarray]:
 
-    spec = spectrogram(audio, window_size, step_size)
+    spec = spectrogram(audio, window_size, step_size, normalize=normalize)
 
     mag = np.abs(spec)
+
     phase = np.angle(spec)
-    phase_delta = np.diff(phase, axis=-1)
+    phase_delta = np.gradient(phase, axis=-1)
     phase = phase_delta % (2 * np.pi)
 
     return mag, phase
@@ -96,6 +127,22 @@ def from_magnitude_and_phase_delta(mag: np.ndarray, phase: np.ndarray) -> np.nda
     accum = (accum * np.pi) % (2 * np.pi) - np.pi
     spec = mag * torch.exp(1j * accum)
     return spec
+
+
+def unit_normalize(x: np.ndarray) -> np.ndarray:
+    return x / (x.max() + 1e-8)
+
+
+def rainbowgram(audio: np.ndarray):
+    mag, phase = to_magnitude_and_phase_delta(audio, normalize=True)
+    mag = unit_normalize(mag)
+    phase = unit_normalize(phase)
+    rg = cm.rainbow(phase)[..., :3]
+    rg *= mag[..., None]
+    rg = unit_normalize(rg)
+    return rg
+
+# rainbowgram
 
 
 """[markdown]
@@ -127,15 +174,79 @@ def from_magnitude_and_phase_delta(mag: np.ndarray, phase: np.ndarray) -> np.nda
  
 """
 
+# aim
+
 def generate_page_dict() -> dict:
 
-    return dict()
+    # source audio ===================================
+    # fetch the source audio
+    audio_example = fetch_audio(
+        'https://music-net.s3.amazonaws.com/2391',
+        start_sample=samplerate * 30)
+    # encode it as a wav file
+    encoded_audio, audio_meta = logger.log_sound('source-audio', audio_example)
+    # create a component for display
+    audio_display = AudioComponent(audio_meta.public_uri, height=200)
+
+    # mag spectrogram ====================================
+    # compute the magnitude spectrogram
+    spec = spectrogram(audio_example, mag_only=True)
+    # encode as an image
+    img_data, spec_meta = logger.log_matrix_with_cmap(
+        'magnitude-spectrogram',
+        np.flipud(spec),
+        cmap='hot')
+    # create a component to display the spec
+    spec_display = ImageComponent(spec_meta.public_uri, height=400)
+
+    # rainbowgram =============================================
+    rg = rainbowgram(audio_example)
+    img_data, rg_meta = logger.log_matrix('rainbowgram', rg[::-1, :, :])
+    rainbowgram_display = ImageComponent(rg_meta.public_uri, height=400)
+
+    # auditory image model ========================================
+    n_filters = 128
+    window_size = 256
+    fb = gammatone_filter_bank(n_filters=n_filters, size=256, device=device, band_spacing='geometric')
+
+    # single_gammatone, gammatone_meta = logger.log
+
+    ta = torch.from_numpy(audio_example).float().to(device).view(1, 1, -1)
+    aim = auditory_image_model(ta, fb, aim_window_size=window_size, aim_step_size=64)
+    batch, channels, time, periodicity = aim.shape
+
+
+    aim = aim.view(channels, time, periodicity).permute(1, 2, 0)
+    print(aim.shape)
+    aim = aim / (aim.max() + 1e-8)
+    aim_movie, aim_meta = logger.log_movie('aim', aim)
+    aim_display = ImageComponent(aim_meta.public_uri, height=400)
+
+
+    return dict(
+        source=audio_display,
+        mag_spec=spec_display,
+        rainbowgram=rainbowgram_display,
+        aim=aim_display)
 
 def generate_article():
+    page_components = generate_page_dict()
     conjure_article(
-        __file__, 'html', title="Phase Invariant Feature", max_depth=1)
+        __file__,
+        'html',
+        title="Phase Invariant Feature",
+        max_depth=1,
+        **page_components)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument(
+        '--clear',
+        action='store_true',
+        required=False,
+        default=False)
     args = parser.parse_args()
-    generate_article()
+    if args.clear:
+        collection.destroy()
+    else:
+        generate_article()
