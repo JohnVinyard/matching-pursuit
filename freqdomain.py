bad003963d02a3952ebf6e65c8a7aef0ead80f4d
John Vinyard <johnwho@gmail.com>
2024-10-31 16:08:39 -0500

    Get freqdomain layered generation working again

--- freqdomain.py
+++ freqdomain.py
@@ -7,23 +7,44 @@ from util import playable
 from util.playable import listen_to_sound
 from torch import nn
 
+
 class Block(nn.Module):
 
-    def __init__(self, window_size: int, transfer: torch.Tensor, gain: Union[float, torch.Tensor]):
+    def __init__(
+            self,
+            window_size: int,
+            control_plane_dim: int,
+            transfer: torch.Tensor,
+            gain: Union[float, torch.Tensor]):
+
         super().__init__()
         self.window_size = window_size
         self.n_coeffs = self.window_size // 2 + 1
+        self.control_plane_dim = control_plane_dim
 
-        self.transfer = nn.Parameter(torch.zeros((self.n_coeffs,)))
+        self.transfer = nn.Parameter(torch.zeros((self.control_plane_dim, self.n_coeffs,)))
         self.transfer.data[:] = transfer
 
         self.gain = nn.Parameter(torch.ones((1,)).fill_(gain))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch, _, time = x.shape
+
+        print('Block input', x.shape)
+        # TODO: mixing matrix
+
+        batch, channels, time = x.shape
+
+        assert channels == self.control_plane_dim
+
         windowed = windowed_audio(x, self.window_size, self.window_size // 2)
+        print('windowed', windowed.shape)
+
         spec = torch.fft.rfft(windowed, dim=-1)
-        batch, _, frames, coeffs = spec.shape
+        print('windowed spec', spec.shape)
+
+        print('transfer', self.transfer.shape)
+
+        batch, channels, frames, coeffs = spec.shape
 
         output_frames = []
 
@@ -33,7 +54,9 @@ class Block(nn.Module):
             if i > 0:
                 current = current + output_frames[i - 1]
 
-            filtered = current * self.transfer[None, None, None, :]
+            # print(current.shape, self.transfer.shape)
+            # TODO: transform the transform function given a deformation matrix
+            filtered = current * self.transfer[None, :, None, :]
 
             output_frames.append(filtered)
 
@@ -42,30 +65,34 @@ class Block(nn.Module):
         samples = overlap_add(audio_windows, apply_window=True)
         samples = samples * self.gain
         samples = torch.tanh(samples)
-        return samples[..., :time]
-
+        x = samples[..., :time]
+        # _, channels, time = x.shape
+        # assert channels == self.control_plane_dim
+        # x = torch.sum(x, dim=1, keepdim=True)
+        return x
 
 
 class AudioNetwork(nn.Module):
-    def __init__(self, window_size: int, n_blocks: int):
+    def __init__(self, control_plane_dim: int, window_size: int, n_blocks: int):
         super().__init__()
         self.window_size = window_size
         self.n_blocks = n_blocks
         self.mixer = nn.Parameter(torch.zeros((n_blocks + 1,)))
+        self.control_plane_dim = control_plane_dim
+        self.n_coeffs = window_size // 2 + 1
 
         self.blocks = nn.ModuleList([
-            Block(window_size, self.init_transfer(), torch.zeros(1).uniform_(10, 200).item())
+            Block(window_size, control_plane_dim, self.init_transfer(), torch.zeros(1).uniform_(1, 10).item())
             for _ in range(self.n_blocks)
         ])
 
     def init_transfer(self):
-        resonances = torch.zeros((batch_size, n_coeffs)).bernoulli_(p=0.01)
-        scaling = torch.zeros_like(resonances).uniform_(0.9, 0.998)
-        pitch = torch.linspace(1, 0, n_coeffs) ** 2
-        scaling = scaling * pitch
-        scaled_resonances = resonances * scaling
+        resonances = torch.zeros(self.control_plane_dim, self.n_coeffs).uniform_(0.5, 0.999)
+        sparse = torch.zeros_like(resonances).bernoulli_(p=0.01)
+        resonances = resonances * sparse
+        scaling = torch.linspace(1, 0, self.n_coeffs) ** 2
+        scaled_resonances = resonances * scaling[None, :]
         return scaled_resonances
-
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         outputs = [x[..., None]]
@@ -75,47 +102,32 @@ class AudioNetwork(nn.Module):
             inp = block(inp)
             outputs.append(inp[..., None])
 
-
         result = torch.cat(outputs, dim=-1)
         mixer_values = torch.softmax(self.mixer, dim=-1)
         mixed = (result * mixer_values[None, None, None, :]).sum(dim=-1)
+        mixed = torch.sum(mixed, dim=1, keepdim=True)
         return mixed
-
 
 
 if __name__ == '__main__':
     batch_size = 1
     window_size = 2048
     n_frames = 128
-    n_coeffs = window_size // 2 + 1
-    gain = 500
+    control_plane_dim = 16
 
 
-    resonances = torch.zeros((batch_size, n_coeffs)).bernoulli_(p=0.01)
-    scaling = torch.zeros_like(resonances).uniform_(0.99, 0.998)
-    pitch = torch.linspace(1, 0, n_coeffs) ** 2
-    scaling = scaling * pitch
-    scaled_resonances = resonances * scaling
-    #
-    # samples = freq_domain_transfer_function_to_resonance(window_size, scaled_resonances, 256, apply_decay=True)
-    # print(samples.min(), samples.max())
-    # samples = samples * gain
-    # samples = torch.tanh(samples)
-    #
-
-    impulse_size = 64
+    impulse_size = 16
     impulse = torch.zeros(impulse_size).uniform_(-1, 1)
     impulse = impulse * torch.hann_window(impulse_size)
 
-    inp = torch.zeros(1, 1, 2**18)
+    inp = torch.zeros(1, control_plane_dim, 2 ** 18)
 
-    inp[:, :, 1024: 1024 + impulse_size] += impulse * 0.1
-    inp[:, :, 16384: 16384 + impulse_size] += impulse
+    inp[:, 0, 1024: 1024 + impulse_size] += impulse * 0.1
+    inp[:, 1, 8192: 8192 + impulse_size] += impulse * 0.5
+    inp[:, 2, 16384: 16384 + impulse_size] += impulse
 
-    network = AudioNetwork(window_size, n_blocks=8)
+    network = AudioNetwork(control_plane_dim, window_size, n_blocks=3)
     samples = network.forward(inp)
-    # block = Block(window_size=window_size, transfer=scaled_resonances, gain=gain)
-    # samples = block.forward(inp)
 
     print(samples.min(), samples.max())
     samples = playable(samples, samplerate=22050, normalize=True)
