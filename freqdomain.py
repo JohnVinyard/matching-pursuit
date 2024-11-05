b87252ca6684b1514f6df881662e43da6d965fd1
John Vinyard <johnwho@gmail.com>
2024-11-05 10:30:27 -0600

    Start working with deformations

--- freqdomain.py
+++ freqdomain.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 import torch
+
+from modules import HyperNetworkLayer
 from modules.overlap_add import overlap_add
 from modules.phase import windowed_audio
 from util import playable
@@ -15,12 +17,23 @@ class Block(nn.Module):
             window_size: int,
             control_plane_dim: int,
             transfer: torch.Tensor,
-            gain: Union[float, torch.Tensor]):
+            gain: Union[float, torch.Tensor],
+            deformation_latent_shape: int = None):
 
         super().__init__()
+        self.deformation_latent_shape = deformation_latent_shape
         self.window_size = window_size
         self.n_coeffs = self.window_size // 2 + 1
         self.control_plane_dim = control_plane_dim
+
+        if self.deformation_latent_shape is not None:
+            self.hyper = HyperNetworkLayer(
+                deformation_latent_shape,
+                deformation_latent_shape,
+                self.n_coeffs,
+                self.n_coeffs,
+                bias=False,
+                force_identity=True)
 
         self.transfer = nn.Parameter(torch.zeros((self.control_plane_dim, self.n_coeffs,)))
         self.mixer_matrix = nn.Parameter(torch.eye(self.control_plane_dim))
@@ -29,7 +42,7 @@ class Block(nn.Module):
 
         self.gain = nn.Parameter(torch.ones((1,)).fill_(gain))
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, deformations: torch.Tensor = None) -> torch.Tensor:
 
         x = (x.permute(0, 2, 1) @ self.mixer_matrix).permute(0, 2, 1)
 
@@ -38,12 +51,8 @@ class Block(nn.Module):
         assert channels == self.control_plane_dim
 
         windowed = windowed_audio(x, self.window_size, self.window_size // 2)
-        print('windowed', windowed.shape)
 
         spec = torch.fft.rfft(windowed, dim=-1)
-        print('windowed spec', spec.shape)
-
-        print('transfer', self.transfer.shape)
 
         batch, channels, frames, coeffs = spec.shape
 
@@ -55,8 +64,17 @@ class Block(nn.Module):
             if i > 0:
                 current = current + output_frames[i - 1]
 
-            # TODO: transform the transform function given a deformation matrix
-            filtered = current * self.transfer[None, :, None, :]
+
+            current_transfer = self.transfer[None, :, None, :]
+
+            if deformations is not None:
+                current_deformation = deformations[:, :, i, :]
+                w, func = self.hyper.forward(current_deformation.view(-1, self.deformation_latent_shape))
+                cdm = func(current_transfer.view(-1, self.n_coeffs))
+                cdm = cdm.view(batch, control_plane_dim, 1, self.n_coeffs)
+                current_transfer = cdm
+
+            filtered = current * current_transfer
 
             output_frames.append(filtered)
 
@@ -70,7 +88,13 @@ class Block(nn.Module):
 
 
 class AudioNetwork(nn.Module):
-    def __init__(self, control_plane_dim: int, window_size: int, n_blocks: int):
+    def __init__(
+            self,
+            control_plane_dim: int,
+            window_size: int,
+            n_blocks: int,
+            deformation_latent_dim: int = None):
+
         super().__init__()
         self.window_size = window_size
         self.n_blocks = n_blocks
@@ -78,8 +102,14 @@ class AudioNetwork(nn.Module):
         self.control_plane_dim = control_plane_dim
         self.n_coeffs = window_size // 2 + 1
 
+
         self.blocks = nn.ModuleList([
-            Block(window_size, control_plane_dim, self.init_transfer(), torch.zeros(1).uniform_(1, 50).item())
+            Block(
+                window_size,
+                control_plane_dim,
+                self.init_transfer(),
+                torch.zeros(1).uniform_(1, 50).item(),
+                deformation_latent_shape=deformation_latent_dim)
             for _ in range(self.n_blocks)
         ])
 
@@ -91,12 +121,12 @@ class AudioNetwork(nn.Module):
         scaled_resonances = resonances * scaling[None, :]
         return scaled_resonances
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, deformations: torch.Tensor) -> torch.Tensor:
         outputs = [x[..., None]]
         inp = x
 
         for block in self.blocks:
-            inp = block(inp)
+            inp = block(inp, deformations)
             outputs.append(inp[..., None])
 
         result = torch.cat(outputs, dim=-1)
@@ -109,21 +139,35 @@ class AudioNetwork(nn.Module):
 if __name__ == '__main__':
     batch_size = 1
     window_size = 2048
-    n_frames = 128
+    step_size = window_size // 2
+    transfer_dim = window_size // 2 + 1
     control_plane_dim = 16
+    low_rank_deformation_dim = 16
+    n_samples = 2 ** 16
+    n_frames = n_samples // step_size
 
+
+    # establish energy inputs (batch, control_plane_dim, n_samples)
     impulse_size = 16
     impulse = torch.zeros(impulse_size).uniform_(-1, 1)
     impulse = impulse * torch.hann_window(impulse_size)
 
-    inp = torch.zeros(1, control_plane_dim, 2 ** 18)
+    inp = torch.zeros(1, control_plane_dim, n_samples)
 
     inp[:, 0, 1024: 1024 + impulse_size] += impulse * 0.1
     inp[:, 1, 8192: 8192 + impulse_size] += impulse * 0.5
     inp[:, 2, 16384: 16384 + impulse_size] += impulse
 
-    network = AudioNetwork(control_plane_dim, window_size, n_blocks=3)
-    samples = network.forward(inp)
+    # establish deformation matrix (batch, control_plane_dim, n_frames, low_rank_deformation_dim)
+    dm = torch.zeros(1, control_plane_dim, n_frames, low_rank_deformation_dim).uniform_(-0.01, 0.01)
+
+    network = AudioNetwork(
+        control_plane_dim,
+        window_size,
+        n_blocks=3,
+        deformation_latent_dim=low_rank_deformation_dim)
+
+    samples = network.forward(inp, dm)
 
     print(samples.min(), samples.max())
     samples = playable(samples, samplerate=22050, normalize=True)
--- modules/hypernetwork.py
+++ modules/hypernetwork.py
@@ -7,20 +7,34 @@ class HyperNetworkLayer(nn.Module):
     Hyper-layer that produces layer weights via a factorized matrix
     """
 
-    def __init__(self, latent_channels, layer_latent, layer_in_channels, layer_out_channels):
+    def __init__(
+            self,
+            latent_channels,
+            layer_latent,
+            layer_in_channels,
+            layer_out_channels,
+            bias: bool = True,
+            force_identity: bool = False):
+
         super().__init__()
         self.latent_channels = latent_channels
         self.layer_latent = layer_latent
         self.layer_in_channels = layer_in_channels
         self.layer_out_channels = layer_out_channels
+        self.force_identity = force_identity
 
-        self.a = nn.Linear(latent_channels, layer_latent * layer_in_channels)
-        self.b = nn.Linear(latent_channels, layer_latent * layer_out_channels)
+        self.a = nn.Linear(latent_channels, layer_latent * layer_in_channels, bias=bias)
+        self.b = nn.Linear(latent_channels, layer_latent * layer_out_channels, bias=bias)
 
     def forward(self, x):
-        a = self.a(x).view(-1, self.layer_in_channels, self.layer_latent)
-        b = self.b(x).view(-1, self.layer_latent, self.layer_out_channels)
-        weights = a @ b
+
+        if self.force_identity:
+            weights = torch.eye(
+                self.layer_in_channels, self.layer_out_channels)[None, ...].repeat(x.shape[0], 1, 1)
+        else:
+            a = self.a(x).view(-1, self.layer_in_channels, self.layer_latent)
+            b = self.b(x).view(-1, self.layer_latent, self.layer_out_channels)
+            weights = a @ b
 
         def forward(z):
             if len(z.shape) != 3:
@@ -29,12 +43,9 @@ class HyperNetworkLayer(nn.Module):
             return z
 
         return weights, forward
-    
-    
 
 
 if __name__ == '__main__':
-
     latent = torch.zeros(7, 13).normal_(0, 1)
 
     model = HyperNetworkLayer(13, 5, 128, 128)
