093a30815bf6bb5536914c9313ddb8967a4e458a
John Vinyard <johnwho@gmail.com>
2025-12-11 17:21:41 -0600

    Slightly better version of simulation

--- simulation.py
+++ simulation.py
@@ -2,10 +2,11 @@ from typing import Tuple
 import torch
 from torch import nn
 
-from modules import stft, unit_norm, flattened_multiband_spectrogram
+from modules import stft, unit_norm, flattened_multiband_spectrogram, interpolate_last_axis
 from modules.decompose import fft_resample
 from modules.overlap_add import overlap_add
-from modules.upsample import upsample_with_holes
+from modules.transfer import fft_convolve
+from modules.upsample import upsample_with_holes, ensure_last_axis_length
 from util.overfit import overfit_model
 
 
@@ -129,9 +130,11 @@ class Model(nn.Module):
             control_frame_rate: int,
             n_samples: int,
             n_filters: int = 64,
-            filter_latent_dim: int = 8):
+            filter_latent_dim: int = 8,
+            filter_size: int = 2048):
         super().__init__()
 
+        self.filter_size = filter_size
         self.n_nodes = n_nodes
         self.node_dim = node_dim
         self.control_frame_rate = control_frame_rate
@@ -142,19 +145,22 @@ class Model(nn.Module):
         self.filter_latent_dim = filter_latent_dim
 
         self.n_coeffs = self.window_size // 2 + 1
-        self.ratio = int((self.n_frames / self.n_samples) * self.n_coeffs)
+
+        n_filters_coeffs = self.filter_size // 2 + 1
+
+        self.ratio = int((self.filter_size / self.n_samples) * n_filters_coeffs)
 
         self.latents = nn.Parameter(torch.zeros(n_nodes, filter_latent_dim).uniform_(-1, 1))
         self.filter_map = nn.Parameter(torch.zeros(filter_latent_dim, n_filters).uniform_(-1, 1))
-        self.filters = nn.Parameter(torch.zeros(n_filters, self.window_size).uniform_(-1, 1))
-        self.high_freq_factor = nn.Parameter(torch.zeros(n_nodes, 1).uniform_(-0.001, 0.001))
+        self.filters = nn.Parameter(torch.zeros(n_filters, self.filter_size).uniform_(-1, 1))
+        self.high_freq_factor = nn.Parameter(torch.zeros(n_nodes, 1).uniform_(-0.00001, 0.00001))
 
         self.nodes = nn.Parameter(torch.zeros(n_nodes, node_dim).uniform_(-1, 1))
         self.masses = nn.Parameter(torch.zeros(n_nodes, ).uniform_(15, 18))
         # TODO: There should be time-varying deformations applied to the tension
         self.tensions = nn.Parameter(torch.zeros(n_nodes, n_nodes).uniform_(10, 11))
 
-        self.damping = 0.95
+        self.damping = 0.98
 
         # How much does each mass contribute to the final recording?
         self.mixer = nn.Parameter(torch.zeros(n_nodes, ).uniform_(-0.1, 0.1))
@@ -164,6 +170,8 @@ class Model(nn.Module):
         self.forces = nn.Parameter(forces)
 
         self.constrained_mask = nn.Parameter(torch.zeros(n_nodes, ).bernoulli_(0.1))
+
+        # self.gains = nn.Parameter(torch.zeros(n_nodes, 1).uniform_(-1, 1))
 
     @property
     def force_norm(self):
@@ -206,35 +214,35 @@ class Model(nn.Module):
     def forward(self) -> torch.Tensor:
         x, node_forces = self._forward()
 
+        envelopes = interpolate_last_axis(node_forces.T, desired_size=self.n_samples)
+
         # map the latents for each node to a linear combination of filteres
         filter_choice = self.filter_map.T @ self.latents.T
-        filters = (self.filters.T @ filter_choice)
+        filters = (self.filters.T @ filter_choice).T
+        filters = ensure_last_axis_length(filters, desired_size=self.n_samples)
+        filters = unit_norm(filters)
+        filters = torch.fft.rfft(filters, dim=-1)
 
-        noise = torch.zeros(1, 1, self.n_nodes, self.n_frames, self.window_size, device=x.device).uniform_(-1, 1)
-        noise_spec = torch.fft.rfft(noise * torch.hamming_window(self.window_size, device=filters.device).view(1, 1, 1, 1, self.window_size), dim=-1)
-        filter_spec = torch.fft.rfft(
-            filters.view(1, 1, self.n_nodes, 1, self.window_size) \
-            * torch.hamming_window(self.window_size, device=noise_spec.device).view(1, 1, 1, 1, self.window_size), dim=-1)
+        # TODO: the cutoff should be all frequencies below the
+        # control/simulation rate
+        filters[..., :self.ratio] = 0
+        filters = torch.fft.irfft(filters, dim=-1)
 
+        noise = torch.zeros_like(envelopes).uniform_(-0.01, 0.01) * envelopes
 
-        conv = noise_spec * filter_spec
+        conv = fft_convolve(filters, noise)
 
-        # ensure that the high-frequency filters don't overlap with the simulation frequency
-        conv[..., :self.ratio] = 0
+        conv = conv * self.high_freq_factor.view(self.n_nodes, 1)
+        conv = conv.view(1, self.n_nodes, self.n_samples)
 
-        conv = torch.fft.irfft(conv, dim=-1)
-        conv = unit_norm(conv, dim=-1)
-        conv = conv * self.high_freq_factor.view(1, 1, self.n_nodes, 1, 1) * node_forces.permute(0, 1).view(1, 1, self.n_nodes, -1, 1)
-        # print(conv.shape, node_forces.shape)
+        # conv = torch.tanh(conv * self.gains)
 
-
-        conv = overlap_add(conv.view(1, self.n_nodes, -1, self.window_size), apply_window=True)[..., : self.n_samples]
         conv = torch.sum(conv, dim=1, keepdim=True)
 
+
         x = x + conv
-        # print(self.latents.shape, node_forces.shape)
-        # filters = self.latents.T @ node_forces.T
-        # print(filters.shape)
+
+
         return x
 
 
@@ -250,7 +258,9 @@ if __name__ == '__main__':
     model = Model(
         n_nodes=128,
         node_dim=2,
-        control_frame_rate=64,
+        control_frame_rate=32,
+        n_filters=128,
+        filter_latent_dim=64,
         n_samples=n_samples
     )
 
@@ -259,7 +269,7 @@ if __name__ == '__main__':
         recon_loss = compute_loss(target, recon)
         energy_loss = model.force_norm
         # encourage sparse energy
-        return recon_loss + (energy_loss * 10)
+        return recon_loss + (energy_loss * 100)
 
 
     overfit_model(
@@ -267,5 +277,5 @@ if __name__ == '__main__':
         model=model,
         loss_func=full_loss_func,
         collection_name='simulation',
-        learning_rate=1e-2
+        learning_rate=1e-3
     )
