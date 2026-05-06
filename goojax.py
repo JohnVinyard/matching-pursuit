d3fea780b7f5ca829f3af717335a3cbadcf5019a
John Vinyard <johnwho@gmail.com>
2026-05-06 17:25:27 -0500

    Refactor to make it possible to generate multiple layers

--- goojax.py
+++ goojax.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Tuple
 
 import torch
@@ -12,15 +13,6 @@ from io import BytesIO
 from soundfile import SoundFile
 from subprocess import Popen, PIPE
 
-# def generate_params(batch_size: int, n_masses: int, dim: int):
-#     key = random.key(1324)
-    
-#     tensions = random.uniform(key, (batch_size, n_masses, dim))
-#     masses = random.uniform(key, (batch_size, n_masses, dim))
-#     pass
-
-# def sim():
-#     pass
 
 
 # TODO: It might be nice to move this into zounds
@@ -48,11 +40,37 @@ type Position = np.ndarray
 type Velocity = np.ndarray
 type Forces = np.ndarray
 type HomePosition = np.ndarray
-
-    
 type Carry = Tuple[Position, Velocity]
-
 type Inputs = Tuple[Forces, HomePosition]
+
+@dataclass
+class LayerParameters:
+    mics: np.ndarray
+    limits: np.ndarray
+    tensions: np.ndarray
+    masses: np.ndarray
+    
+
+def create_layer_parameters(batch_size: int, n_masses: int, dim: int) -> LayerParameters:
+    rk = random.key(int(time()))
+    rk, t, m, mic, l = random.split(rk, 5)
+    
+    
+    # TODO: add a utility to generate a layer
+    # direction of mics, which project ND vibration into a 1D signal
+    mics = random.uniform(mic, (1, batch_size, n_masses, dim, 1), minval=-0.01, maxval=0.01)
+    limits = random.uniform(l, (1, n_masses, dim), minval=-0.01, maxval=0.01)
+    tensions = random.uniform(t, (1, n_masses, dim), minval=0.00001, maxval=5)
+    masses = random.uniform(m, (1, n_masses, 1), minval=100, maxval=500)
+    
+    return LayerParameters(
+        mics=mics,
+        limits=limits,
+        tensions=tensions,
+        masses=masses
+    )
+    
+
 
 def tryjax():
     
@@ -69,21 +87,16 @@ def tryjax():
     n_samples = 2 ** 16
     
     rk = random.key(int(time()))
-    rk, f, t, m, mic, l = random.split(rk, 6)
+    rk, f, t = random.split(rk, 3)
+    
+    layer_params = create_layer_parameters(batch_size, n_masses, dim)
+    
+    mics = layer_params.mics
+    tensions = layer_params.tensions
+    masses = layer_params.masses
+    limits = layer_params.limits
     
     damping = 0.9998
-    
-    # TODO: add a utility to generate a layer
-    # direction of mics, which project ND vibration into a 1D signal
-    mics = random.uniform(mic, (1, batch_size, n_masses, dim, 1), minval=-0.01, maxval=0.01)
-    limits = random.uniform(l, (1, n_masses, dim), minval=-0.01, maxval=0.01)
-    tensions = random.uniform(t, (1, n_masses, dim), minval=0.00001, maxval=5)
-    masses = random.uniform(m, (1, n_masses, 1), minval=100, maxval=500)
-    
-    
-    # home_pos = np.zeros((1, n_masses, dim))
-    
-    
     
     
     # TODO: add a time-varying home-position here
@@ -128,8 +141,10 @@ def tryjax():
     
     forces = random.bernoulli(f, p=1e-5, shape=(n_samples, batch_size, n_masses, 1))
     
-    
+    # per-sample position that the nodes are trying to return to
+    # via  a spring mechanism
     home_pos = np.zeros((n_samples, batch_size, n_masses, dim))
+    
     forces = forces * random.uniform(f, shape=(n_samples, batch_size, n_masses, dim ), minval=-0.1, maxval=0.1)
     
     inputs = (forces, home_pos)
@@ -164,7 +179,7 @@ def tryjax():
     
     _, _, spec = sp.signal.stft(normalized_samples[0], nperseg=2048, noverlap=256)
     
-    spec = np.log(1e-12 + np.abs(spec))
+    spec = np.abs(spec)
     
     
     plt.matshow(np.flipud(spec[:256, :]))
